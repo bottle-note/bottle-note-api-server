16b6b94150099255364ed2c04ea039400f33e6ba
deadWhale <rlagusrl928@gmail.com>
2024-02-27 14:23:55 +0900

    project default setup

---
+++ src/main/java/app/bottlenote/BottleNoteApplication.java
@@ -0,0 +1,11 @@
+package app.bottlenote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BottleNoteApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BottleNoteApplication.class, args);
+    }
+}
