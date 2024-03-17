96fcf07f49292686dca0812ac7f994f9ff0f85e4
KIM HYUN GI <rlagusrl928@gmail.com>
2024-03-17 20:36:04 +0900

    feat: create GlobalResponse

--- src/main/java/app/bottlenote/global/response/GlobalResponse.java
+++ src/main/java/app/bottlenote/global/response/GlobalResponse.java
@@ -3,18 +3,71 @@ package app.bottlenote.global.response;
 import lombok.Builder;
 import lombok.Getter;
 
-import java.util.List;
+import static java.util.Collections.emptyList;
 
+/**
+ * 전역 응답 객체
+ */
 @Getter
 public class GlobalResponse {
 	private final String success;
 	private final Integer code;
-	private final List<?> data;
-	private final List<?> errors;
-	private final List<?> meta;
+	private final Object data;
+	private final Object errors;
+	private final Object meta;
+
+	public static GlobalResponse success(Object data) {
+		return GlobalResponse.builder()
+			.success("true")
+			.code(200)
+			.errors(emptyList())
+			.meta(emptyList())
+			.data(data)
+			.build();
+	}
+
+	public static GlobalResponse success(Object data, Object meta) {
+		return GlobalResponse.builder()
+			.success("true")
+			.code(200)
+			.errors(emptyList())
+			.meta(meta)
+			.data(data)
+			.build();
+	}
+
+	public static GlobalResponse fail(Integer code, Object errors) {
+		return GlobalResponse.builder()
+			.success("fail")
+			.code(code)
+			.data(emptyList())
+			.errors(errors)
+			.meta(emptyList())
+			.build();
+	}
+
+	public static GlobalResponse fail(Integer code, Object errors, Object meta) {
+		return GlobalResponse.builder()
+			.success("fail")
+			.code(code)
+			.data(emptyList())
+			.errors(errors)
+			.meta(meta)
+			.build();
+	}
+
+	public static GlobalResponse error(Integer code, Object errors) {
+		return GlobalResponse.builder()
+			.success("error")
+			.code(code)
+			.data(emptyList())
+			.errors(errors)
+			.meta(emptyList())
+			.build();
+	}
 
 	@Builder
-	public GlobalResponse(String success, Integer code, List<?> data, List<?> errors, List<?> meta) {
+	private GlobalResponse(String success, Integer code, Object data, Object errors, Object meta) {
 		this.success = success;
 		this.code = code;
 		this.data = data;
