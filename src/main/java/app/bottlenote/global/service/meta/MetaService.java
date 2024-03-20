bf6b5bf97a15da3211d909270364eb0cd08b1020
KIM HYUN GI <rlagusrl928@gmail.com>
2024-03-20 22:37:01 +0900

    feat: MeatInfos 구현헤 메타 정보 관리 유연성을 추가

--- src/main/java/app/bottlenote/global/service/meta/MetaService.java
+++ src/main/java/app/bottlenote/global/service/meta/MetaService.java
@@ -1,14 +1,18 @@
-package app.bottlenote.common.service;
+package app.bottlenote.global.service.meta;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
 import java.time.ZoneId;
-import java.util.HashMap;
-import java.util.Map;
 
+/**
+ * 메타 정보를 관리하기 위한 서비스입니다.<br>
+ * 추가적인 메타 정보를 생성하고 반환할 수 있습니다.
+ */
+@Slf4j
 @Component
 public class MetaService {
 
@@ -16,13 +20,17 @@ public class MetaService {
 	public static String serverPathVersion;
 	private static String serverEncoding;
 
+	/**
+	 * appllication.yml 의 설정값을 주입한다.
+	 *
+	 * @param env the env
+	 */
 	@Autowired
 	public void setEnvironment(Environment env) {
 		serverVersion = env.getProperty("server.version");
 		serverPathVersion = env.getProperty("server.path.version");
 		serverEncoding = env.getProperty("server.encoding.charset");
 	}
-
 
 	/**
 	 * 메타 정보를 생성한다.
@@ -32,12 +40,14 @@ public class MetaService {
 	 * - server encoding : 서버 인코딩<br>
 	 * - server time : 서버 시간<br>
 	 */
-	public static Map<String, String> createMetaInfo() {
-		HashMap<String, String> meta = new HashMap<>();
-		meta.put("server_version", serverVersion);
-		meta.put("server_path_version", serverPathVersion);
-		meta.put("server_encoding", serverEncoding);
-		meta.put("server_response_time", LocalDateTime.now(ZoneId.of("Asia/Seoul")).toString());
-		return meta;
+	public static MetaInfos createMetaInfo() {
+		MetaInfos metaInfos = new MetaInfos();
+
+		metaInfos.add("server_version", serverVersion);
+		metaInfos.add("server_path_version", serverPathVersion);
+		metaInfos.add("server_encoding", serverEncoding);
+		metaInfos.add("server_response_time", LocalDateTime.now(ZoneId.of("Asia/Seoul")).toString());
+
+		return metaInfos;
 	}
 }
