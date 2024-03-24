d31234ce7eb333b7b9cb2a340961113b452dd9a9
whale0928 <rlagusrl928@gmail.com>
2024-03-24 17:02:01 +0900

    feat: Abstract Custom Exception class 구현

---
+++ src/main/java/app/bottlenote/global/exception/custom/AbstractCustomException.java
@@ -0,0 +1,21 @@
+package app.bottlenote.global.exception.custom;
+
+import app.bottlenote.global.exception.custom.code.ExceptionCode;
+import lombok.Getter;
+
+
+
+@Getter
+public abstract class AbstractCustomException extends RuntimeException {
+	private final ExceptionCode exceptionCode;
+
+	public AbstractCustomException(ExceptionCode exceptionCode) {
+		super(exceptionCode.getMessage());
+		this.exceptionCode = exceptionCode;
+	}
+
+	@Override
+	public String getMessage() {
+		return exceptionCode.getMessage();
+	}
+}
