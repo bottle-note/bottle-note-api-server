9b5ba6a2215cf786b066748572a93df80fa4620d
whale0928 <rlagusrl928@gmail.com>
2024-03-24 17:17:41 +0900

    hotfix: fixed Set in Map -> Only Map

--- src/main/java/app/bottlenote/global/service/meta/MetaInfos.java
+++ src/main/java/app/bottlenote/global/service/meta/MetaInfos.java
@@ -2,43 +2,28 @@ package app.bottlenote.global.service.meta;
 
 import lombok.Getter;
 
-import java.util.HashSet;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 
 
 @Getter
 public class MetaInfos {
-	private final HashSet<Map<String, Object>> metaInfos;
+	private final Map<String, Object> metaInfos;
 
 	protected MetaInfos() {
-		metaInfos = new HashSet<>();
+		metaInfos = new HashMap<>();
 	}
 
-	/**
-	 * 추가적인 메타 정보를 추가한다.
-	 *
-	 * @param key   the key
-	 * @param value the value
-	 */
-	public MetaInfos add(String key, Object value) {
-		Objects.requireNonNull(key, "메타정보의 key는 null이 될 수 없습니다.");
-		Objects.requireNonNull(value, "메타정보의 value는 null이 될 수 없습니다.");
+	public Map<String, Object> add(String key, Object value) {
+		Objects.requireNonNull(key, "key는 null이 될 수 없습니다.");
+		Objects.requireNonNull(value, "value 는 null이 될 수 없습니다.");
 
-		metaInfos.add( Map.of(key, value));
-		return this;
+		metaInfos.put(key, value);
+		return metaInfos;
 	}
 
-	/**
-	 * 메타 정보에서 key에 해당하는 값을 찾아 반환한다.
-	 *
-	 * @param key 검색 대상이 될 key값
-	 * @return the map
-	 */
-	public Map<String, ?> findByKey(String key) {
-		return metaInfos.stream()
-			.filter(metaInfo -> metaInfo.containsKey(key))
-			.findFirst()
-			.orElse(Map.of());
+	public Object findByKey(String key) {
+		return metaInfos.get(key);
 	}
 }
