d4ae39bef3e817ff17b13d87ac586c7faab7c0c0
juye <kjy_1615@naver.com>
2024-03-28 21:56:21 +0900

    feat : add OauthLoginContoller

---
+++ src/main/java/app/bottlenote/oauth/controller/OauthController.java
@@ -0,0 +1,26 @@
+package app.bottlenote.oauth.controller;
+
+import app.bottlenote.oauth.dto.OauthLoginRequest;
+import app.bottlenote.oauth.service.OauthLoginService;
+import app.bottlenote.security.JwtDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/oauth")
+public class OauthController {
+
+	private final OauthLoginService oauthLoginService;
+
+
+    public ResponseEntity<JwtDto> OauthLogin(@RequestBody OauthLoginRequest OauthLoginReq){
+		return ResponseEntity.ok(oauthLoginService.OauthLogin(OauthLoginReq));
+	}
+
+}
