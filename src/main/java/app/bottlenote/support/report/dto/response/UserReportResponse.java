6de5bc70b16baf9695e2a70bd3eda0f79014377f
whale0928 <rlagusrl928@gmail.com>
2024-04-14 00:12:25 +0900

    fix: snake_case to camelCase

--- src/main/java/app/bottlenote/support/report/dto/response/UserReportResponse.java
+++ src/main/java/app/bottlenote/support/report/dto/response/UserReportResponse.java
@@ -6,17 +6,17 @@ import lombok.Getter;
 @Getter
 public class UserReportResponse {
 	private final String message; //: "신고가 접수되었습니다.",
-	private final Long report_user_id; //: 3,
-	private final String report_user_name; //: "신고 대상 이름"
+	private final Long reportUserId; //: 3,
+	private final String reportUserName; //: "신고 대상 이름"
 
-	protected UserReportResponse(String message, Long report_user_id, String report_user_name) {
+	protected UserReportResponse(String message, Long reportUserId, String reportUserName) {
 		this.message = message;
-		this.report_user_id = report_user_id;
-		this.report_user_name = report_user_name;
+		this.reportUserId = reportUserId;
+		this.reportUserName = reportUserName;
 	}
 
-	public static UserReportResponse of(Long report_user_id, String report_user_name) {
-		return new UserReportResponse(UserReportResponseEnum.SUCCESS.getMessage(), report_user_id, report_user_name);
+	public static UserReportResponse of(Long reportUserId, String reportUserName) {
+		return new UserReportResponse(UserReportResponseEnum.SUCCESS.getMessage(), reportUserId, reportUserName);
 	}
 
 	@AllArgsConstructor
