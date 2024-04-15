ed0a88ebded5b2666a0d77bae0f5edbbf60e9601
chadongmin <cdm2883@naver.com>
2024-04-15 16:35:14 +0900

    fix : init-sql CREATE 구문에서 id AUTO_INCREMENT 속성 추가 및 BaseTimeEntity 관련 컬럼 NULL 허용

--- database/mysql/init/init-tasting-tag.sql
+++ database/mysql/init/init-tasting-tag.sql
@@ -1,19 +1,19 @@
 CREATE TABLE `tasting_tag`
 (
-    `id`             bigint       NOT NULL,
+    `id`             bigint       NOT NULL AUTO_INCREMENT,
     `kor_name`       VARCHAR(255) NOT NULL,
     `eng_name`       VARCHAR(255) NOT NULL,
     `icon`           VARCHAR(255) NULL,
-    `create_at`      TIMESTAMP    NOT NULL,
-    `last_modify_at` TIMESTAMP    NOT NULL,
-    `create_by`      VARCHAR(255) NOT NULL,
-    `last_modify_by` VARCHAR(255) NOT NULL,
+    `create_at`      TIMESTAMP    NULL,
+    `last_modify_at` TIMESTAMP    NULL,
+    `create_by`      VARCHAR(255) NULL,
+    `last_modify_by` VARCHAR(255) NULL,
     PRIMARY KEY (`id`)
 );
 
 
 CREATE TABLE `alcohols_tasting_tags`(
-    `id`             bigint NOT NULL,
+    `id`             bigint NOT NULL AUTO_INCREMENT,
     `alcohols_id`    bigint NOT NULL,
     `tasting_tag_id` bigint NOT NULL,
     PRIMARY KEY (`id`)
