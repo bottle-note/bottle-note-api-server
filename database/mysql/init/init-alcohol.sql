33e1961e3c029ad3515dca126edc40160dd0d795
chadongmin <cdm2883@naver.com>
2024-04-15 16:19:21 +0900

    fix : init-sql CREATE 구문에서 id AUTO_INCREMENT 속성 추가 및 BaseTimeEntity 관련 컬럼 NULL 허용

--- database/mysql/init/init-alcohol.sql
+++ database/mysql/init/init-alcohol.sql
@@ -1,5 +1,5 @@
 CREATE TABLE `alcohol`(
-    `id`             bigint       NOT NULL,
+    `id`             bigint       NOT NULL AUTO_INCREMENT,
     `eng_name`       VARCHAR(255) NOT NULL COMMENT '영문 이름',
     `kor_name`       VARCHAR(255) NOT NULL COMMENT '한글 이름',
     `abv`            VARCHAR(255) NOT NULL COMMENT '도수',
@@ -9,30 +9,30 @@ CREATE TABLE `alcohol`(
     `distillery_id`  BIGINT NOT NULL COMMENT '증류소 정보',
     `tier_id`        BIGINT NOT NULL COMMENT '등급 정보',
     `cask`           VARCHAR(255) NULL COMMENT '캐스트 타입(단순 문자열로 박기)',
-    `create_at`      TIMESTAMP    NOT NULL,
-    `last_modify_at` TIMESTAMP    NOT NULL,
-    `create_by`      VARCHAR(255) NOT NULL,
-    `last_modify_by` VARCHAR(255) NOT NULL,
+    `create_at`      TIMESTAMP    NULL,
+    `last_modify_at` TIMESTAMP    NULL,
+    `create_by`      VARCHAR(255) NULL,
+    `last_modify_by` VARCHAR(255) NULL,
     PRIMARY KEY (`id`)
 );
 
 
 CREATE TABLE `tier`
 (
-    `id`             bigint       NOT NULL,
+    `id`             bigint       NOT NULL AUTO_INCREMENT,
     `type`           VARCHAR(255) NOT NULL COMMENT '위스키 / 꼬냑 / 진',
     `tier`           VARCHAR(255) NOT NULL COMMENT '등급명',
-    `last_modify_at` TIMESTAMP    NOT NULL,
-    `last_modify_by` VARCHAR(255) NOT NULL,
-    `create_at`      TIMESTAMP    NOT NULL,
-    `create_by`      VARCHAR(255) NOT NULL,
+    `last_modify_at` TIMESTAMP    NULL,
+    `last_modify_by` VARCHAR(255) NULL,
+    `create_at`      TIMESTAMP    NULL,
+    `create_by`      VARCHAR(255) NULL,
     PRIMARY KEY (`id`)
 );
 
 
 CREATE TABLE `country`
 (
-    `id`             bigint       NOT NULL COMMENT 'https://www.data.go.kr/data/15076566/fileData.do?recommendDataYn=Y',
+    `id`             bigint       NOT NULL AUTO_INCREMENT COMMENT 'https://www.data.go.kr/data/15076566/fileData.do?recommendDataYn=Y',
     `kor_name`       VARCHAR(255) NOT NULL,
     `eng_name`       VARCHAR(255) NOT NULL,
     `iso_alpha2`     VARCHAR(255) NULL,
@@ -44,13 +44,13 @@ CREATE TABLE `country`
 
 CREATE TABLE `distillery`
 (
-    `id`             bigint       NOT NULL,
+    `id`             bigint       NOT NULL AUTO_INCREMENT,
     `kor_name`       VARCHAR(255) NOT NULL,
     `eng_name`       VARCHAR(255) NOT NULL,
     `logo_img_path`  VARCHAR(255) NULL,
-    `last_modify_at` TIMESTAMP    NOT NULL,
-    `last_modify_by` VARCHAR(255)    NOT NULL,
-    `create_at`      TIMESTAMP    NOT NULL,
-    `create_by`      VARCHAR(255)    NOT NULL,
+    `last_modify_at` TIMESTAMP    NULL,
+    `last_modify_by` VARCHAR(255) NULL,
+    `create_at`      TIMESTAMP    NULL,
+    `create_by`      VARCHAR(255) NULL,
     PRIMARY KEY (`id`)
 );
