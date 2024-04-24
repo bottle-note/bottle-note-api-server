121e3bcf0dd9f17a92f1c139f11cb3feb379c5eb
whale0928 <rlagusrl928@gmail.com>
2024-04-25 03:51:23 +0900

    feat: update query

--- src/main/java/app/bottlenote/alcohols/repository/PopularQueryRepository.java
+++ src/main/java/app/bottlenote/alcohols/repository/PopularQueryRepository.java
@@ -14,7 +14,11 @@ public interface PopularQueryRepository extends CrudRepository<Alcohol, Long> {
 	@Query("select new app.bottlenote.alcohols.dto.response.Populars(a.id, a.korName, a.engName, sum(r.rating), a.category, a.imageUrl) " +
 		"from alcohol a " +
 		"left outer join rating r on a.id = r.alcohol.id " +
-		"group by a.id, a.korName, a.engName, a.category, a.imageUrl ")
+		"where r.createAt >= current_date - 7 " +
+		"group by a.id, a.korName, a.engName, a.category, a.imageUrl " +
+		"having sum(r.rating) > 0 " +
+		"order by sum(r.rating) desc " +
+		"limit :top")
 	List<Populars> getPopularOfWeek(Integer top);
 
 }
