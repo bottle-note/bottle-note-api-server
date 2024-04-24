74247c2615a531cb39ae3450fcd4bfdea67d1042
whale0928 <rlagusrl928@gmail.com>
2024-04-25 01:15:32 +0900

    fix: Populars type update

--- src/main/java/app/bottlenote/alcohols/dto/response/Populars.java
+++ src/main/java/app/bottlenote/alcohols/dto/response/Populars.java
@@ -1,35 +1,20 @@
 package app.bottlenote.alcohols.dto.response;
 
-import lombok.Builder;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class Populars {
-	private final String whiskyId;  // 1,
-	private final String korName;  // '글렌피딕',
-	private final String engName;  // 'glen fi',
-	private final String rating;  // 3.5,
-	private final String category;  // 'single molt',
-	private final String imagePath;  // "https://i.imgur.com/TE2nmYV.png"
+    private final Long whiskyId;  // 1,
+    private final String korName;  // '글렌피딕',
+    private final String engName;  // 'glen fi',
+    private final Double rating;  // 3.5,
+    private final String category;  // 'single molt',
+    private final String imagePath;  // "https://i.imgur.com/TE2nmYV.png"
 
-	@Builder
-	public Populars(String whiskyId, String korName, String engName, String rating, String category, String imagePath) {
-		this.whiskyId = whiskyId;
-		this.korName = korName;
-		this.engName = engName;
-		this.rating = rating;
-		this.category = category;
-		this.imagePath = imagePath;
-	}
-
-	public static Populars of(String whiskyId, String korName, String engName, String rating, String category, String imagePath) {
-		return Populars.builder()
-			.whiskyId(whiskyId)
-			.korName(korName)
-			.engName(engName)
-			.rating(rating)
-			.category(category)
-			.imagePath(imagePath)
-			.build();
-	}
+    public static Populars of(Long whiskyId, String korName, String engName, Double rating, String category, String imagePath) {
+        return new Populars(whiskyId, korName, engName, rating, category, imagePath);
+    }
 }
--- src/main/java/app/bottlenote/alcohols/service/PopularService.java
+++ src/main/java/app/bottlenote/alcohols/service/PopularService.java
@@ -17,11 +17,11 @@ public class PopularService {
 	@Transactional
 	public List<Populars> getPopularOfWeek(Integer top) {
 		List<Populars> populars = List.of(
-			Populars.of("1", "글렌피딕", "glen fi", "3.5", "single molt", "https://i.imgur.com/TE2nmYV.png"),
-			Populars.of("2", "맥키토시", "macintosh", "4.5", "single molt", "https://i.imgur.com/TE2nmYV.png"),
-			Populars.of("3", "글렌리벳", "glen rivet", "4.0", "single molt", "https://i.imgur.com/TE2nmYV.png"),
-			Populars.of("4", "글렌피딕", "glen fi", "3.5", "single molt", "https://i.imgur.com/TE2nmYV.png"),
-			Populars.of("5", "맥키토시", "macintosh", "4.5", "single molt", "https://i.imgur.com/TE2nmYV.png")
+			Populars.of(1L, "글렌피딕", "glen fi", 3.5, "single molt", "https://i.imgur.com/TE2nmYV.png"),
+			Populars.of(2L, "맥키토시", "macintosh", 4.5, "single molt", "https://i.imgur.com/TE2nmYV.png"),
+			Populars.of(3L, "글렌리벳", "glen rivet", 4.0, "single molt", "https://i.imgur.com/TE2nmYV.png"),
+			Populars.of(4L, "글렌피딕", "glen fi", 3.5, "single molt", "https://i.imgur.com/TE2nmYV.png"),
+			Populars.of(5L, "맥키토시", "macintosh", 4.5, "single molt", "https://i.imgur.com/TE2nmYV.png")
 		);
 		return populars;
 	}
