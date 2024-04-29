b31987c51cf1ede38bece9197b11ad4f911fd331
chadongmin <cdm2883@naver.comn>
2024-04-29 19:50:54 +0900

    fix : 로그인 API 엔드포인트 수정 (POST요청)

--- src/main/java/app/bottlenote/user/controller/OauthController.java
+++ src/main/java/app/bottlenote/user/controller/OauthController.java
@@ -7,20 +7,21 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-//@RequestMapping("/api/v1/oauth")
+@RequestMapping("/api/v1/oauth")
 public class OauthController {
 
 	private final OauthService oauthService;
 
-	@GetMapping("/login")
+	@PostMapping("/login")
 	public ResponseEntity<GlobalResponse> oauthLogin(@RequestBody @Valid OauthRequest oauthReq) {
 		log.info("Received oauth request: {}", oauthReq); // Log the request for debugging
 		return ResponseEntity.ok(
