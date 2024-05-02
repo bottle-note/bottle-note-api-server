6abbafbd3b58a0071e5547ef13cf906311892230
chadongmin <cdm2883@naver.comn>
2024-05-02 22:12:55 +0900

    fix : DTO 클래스 바인딩 에러 수정 및 불필요한 어노테이션 제거

--- src/main/java/app/bottlenote/user/dto/request/OauthRequest.java
+++ src/main/java/app/bottlenote/user/dto/request/OauthRequest.java
@@ -6,26 +6,29 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class OauthRequest {
 
 	@NotBlank(message = "로그인 및 회원가입에 필요한 이메일이 없습니다.")
 	@Email(message = "올바른 이메일형식이 아닙니다.")
-	private final String email;
+	private String email;
 
 	@NotNull(message = "로그인 및 회원가입에 필요한 소셜타입이 없습니다.")
-	private final SocialType socialType;
+	private SocialType socialType;
 
-	private final GenderType gender;
+	private GenderType gender;
 
 	@Min(value = 0, message = "나이가 잘못 입력됐습니다.")
-	private final int age;
+	private Integer age;
 
 	@Builder
-	public OauthRequest(String email, GenderType gender, int age, SocialType socialType) {
+	public OauthRequest(String email, GenderType gender, Integer age, SocialType socialType) {
 		this.email = email;
 		this.gender = gender;
 		this.age = age;
