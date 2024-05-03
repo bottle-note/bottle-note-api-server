2e2b9e995b2f035a8accf6679ad15222ac07ce56
chadongmin <cdm2883@naver.comn>
2024-05-03 17:49:35 +0900

    feat : JwtAuthenticationFilter 구현

---
+++ src/main/java/app/bottlenote/common/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,42 @@
+package app.bottlenote.common.jwt;
+
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+	public static final String AUTHORIZATION_HEADER = "Authorization";
+	public static final String BEARER_PREFIX = "Bearer ";
+
+	private final JwtTokenProvider jwtTokenProvider;
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+		FilterChain filterChain) throws ServletException, IOException {
+		String token = resolveToken(request);
+		if (token != null && !token.trim().isEmpty() && jwtTokenProvider.validateToken(token)) {
+			Authentication authentication = jwtTokenProvider.getAuthentication(token);
+			SecurityContextHolder.getContext().setAuthentication(authentication);
+		}
+		filterChain.doFilter(request, response);
+	}
+
+	private String resolveToken(HttpServletRequest request) {
+		String bearerToken = request.getHeader(AUTHORIZATION_HEADER);
+		if (bearerToken != null && !bearerToken.trim().isEmpty() && bearerToken.startsWith(
+			BEARER_PREFIX)) {
+			return bearerToken.split(" ")[1].trim();
+		}
+		return null;
+	}
+
+}
