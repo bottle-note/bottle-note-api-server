429ec73a19cb0996f8d1a0d233f4877ff3782df9
chadongmin <cdm2883@naver.comn>
2024-05-03 22:38:21 +0900

    fix : 토큰 유효성 검증 메서드 분리 및 HttpRequest에서 토큰 추출하는 static 메서드 생성

--- src/main/java/app/bottlenote/common/jwt/JwtAuthenticationFilter.java
+++ src/main/java/app/bottlenote/common/jwt/JwtAuthenticationFilter.java
@@ -7,10 +7,12 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+@Slf4j
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
@@ -23,7 +25,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
 		FilterChain filterChain) throws ServletException, IOException {
 		String token = resolveToken(request);
-		if (token != null && jwtTokenProvider.validateToken(token)) {
+		if (isValidToken(token)) {
 			Authentication authentication = jwtTokenProvider.getAuthentication(token);
 			SecurityContextHolder.getContext().setAuthentication(authentication);
 		}
@@ -32,13 +34,26 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
 	private String resolveToken(HttpServletRequest request) {
 		String bearerToken = request.getHeader(AUTHORIZATION_HEADER);
-		if (bearerToken != null && !bearerToken.trim().isEmpty() && bearerToken.startsWith(
-			BEARER_PREFIX)) {
 
-			//TODO : 추후 static 메서드로 분리하여 재사용
-			return bearerToken.split(" ")[1].trim();
+		//resolveToken 메서드는 request에서 요청을 추출하는 책임만을 갖고 있기 떄문에 기본적인 검사만 수행
+		if (bearerToken != null && bearerToken.startsWith(BEARER_PREFIX)) {
+			return extractToken(bearerToken);
 		}
 		return null;
+	}
+
+	/**
+	 * 추출된 토큰이 실제 유효한 토큰인지 검증.
+	 */
+	private boolean isValidToken(String token) {
+		return jwtTokenProvider.validateToken(token) && token != null && !token.trim().isEmpty();
+	}
+
+	/**
+	 * 요청에서 토큰만 추출하는 메서드
+	 */
+	public static String extractToken(String request) {
+		return request.substring(BEARER_PREFIX.length()).trim();
 	}
 
 }
