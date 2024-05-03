9ce351911dddc5880df36957e9ca3d01b4ff0f5f
chadongmin <cdm2883@naver.comn>
2024-05-03 19:19:12 +0900

    fix : 불필요한 null 체크 삭제

--- src/main/java/app/bottlenote/common/jwt/JwtAuthenticationFilter.java
+++ src/main/java/app/bottlenote/common/jwt/JwtAuthenticationFilter.java
@@ -23,7 +23,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
 		FilterChain filterChain) throws ServletException, IOException {
 		String token = resolveToken(request);
-		if (token != null && !token.trim().isEmpty() && jwtTokenProvider.validateToken(token)) {
+		if (token != null && jwtTokenProvider.validateToken(token)) {
 			Authentication authentication = jwtTokenProvider.getAuthentication(token);
 			SecurityContextHolder.getContext().setAuthentication(authentication);
 		}
