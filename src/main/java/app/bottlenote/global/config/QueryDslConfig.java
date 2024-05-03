c9c35e37cfce08c6a78960b07590942c0b8f7eda
whale0928 <rlagusrl928@gmail.com>
2024-05-04 05:04:12 +0900

    feat: CursorPageable 객체 서비스 구현

--- src/main/java/app/bottlenote/global/config/QueryDslConfig.java
+++ src/main/java/app/bottlenote/global/config/QueryDslConfig.java
@@ -2,15 +2,14 @@ package app.bottlenote.global.config;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
-import lombok.RequiredArgsConstructor;
+import jakarta.persistence.PersistenceContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@RequiredArgsConstructor
 public class QueryDslConfig {
-
-	private final EntityManager em;
+	@PersistenceContext
+	private EntityManager em;
 
 	@Bean
 	public JPAQueryFactory jpaQueryFactory() {
