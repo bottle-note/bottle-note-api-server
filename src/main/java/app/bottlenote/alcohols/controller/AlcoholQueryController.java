8c1b2e26f6ad84f35273de0bd41c94d6b359d121
whale0928 <rlagusrl928@gmail.com>
2024-05-04 05:08:46 +0900

    feat: implement custom repository

--- src/main/java/app/bottlenote/alcohols/controller/AlcoholQueryController.java
+++ src/main/java/app/bottlenote/alcohols/controller/AlcoholQueryController.java
@@ -4,13 +4,13 @@ package app.bottlenote.alcohols.controller;
 import app.bottlenote.alcohols.dto.request.AlcoholSearchRequest;
 import app.bottlenote.alcohols.service.AlcoholQueryService;
 import app.bottlenote.global.data.response.GlobalResponse;
-import app.bottlenote.global.service.meta.MetaInfos;
+import app.bottlenote.global.service.cursor.PageResponse;
 import app.bottlenote.global.service.meta.MetaService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,21 +22,31 @@ public class AlcoholQueryController {
 
 	private final AlcoholQueryService alcoholQueryService;
 
+	/**
+	 * 위스키를 검색하는 API 입니다.
+	 * 사용자가 있을 경우 좋아요 여부도 함께 전달합니다.
+	 *
+	 * @param request the request
+	 * @param token   the token
+	 * @return the response entity
+	 */
 	@GetMapping
 	public ResponseEntity<GlobalResponse> searchAlcohols(
-		@RequestBody @Valid AlcoholSearchRequest request,
+		@ModelAttribute @Valid AlcoholSearchRequest request,
 		@RequestHeader(value = "Authorization", required = false) String token
 	) {
-		Long id = 1L;
+		Long id = 1L; //todo: token 에서 id 추출 로직으로 변경 필요
 
-		MetaInfos metaInfo = MetaService.createMetaInfo()
-			.add("search", request)
-			.add("token", token);
+		PageResponse<?> pageResponse = alcoholQueryService.searchAlcohols(request, id);
 
 		return ResponseEntity.ok(GlobalResponse
 			.success(
-				alcoholQueryService.searchAlcohols(request, id),
-				metaInfo
+				//details.getContent(),
+				pageResponse.content(),
+				MetaService.createMetaInfo()
+					.add("searchParameters", request)
+					.add("pageable", pageResponse.cursorPageable())
+					.add("token", token) //삭제 예정
 			)
 		);
 	}
--- src/main/java/app/bottlenote/alcohols/service/AlcoholQueryService.java
+++ src/main/java/app/bottlenote/alcohols/service/AlcoholQueryService.java
@@ -1,27 +1,24 @@
 package app.bottlenote.alcohols.service;
 
+import app.bottlenote.alcohols.dto.dsl.AlcoholSearchCriteria;
 import app.bottlenote.alcohols.dto.request.AlcoholSearchRequest;
-import app.bottlenote.alcohols.dto.response.AlcoholSearchResponse;
+import app.bottlenote.alcohols.repository.AlcoholQueryRepository;
+import app.bottlenote.global.service.cursor.PageResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
-
-import static app.bottlenote.alcohols.dto.response.AlcoholSearchResponse.SearchDetail;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class AlcoholQueryService {
 
-	public AlcoholSearchResponse searchAlcohols(AlcoholSearchRequest request, Long id) {
-		List<SearchDetail> searchDetails = List.of(
-			new SearchDetail("imageUrl", 1L, "korName1", "engName1", "categoryName1", null, 1L, false),
-			new SearchDetail("imageUrl", 2L, "korName2", "engName2", "categoryName2", null, 1L, false),
-			new SearchDetail("imageUrl", 3L, "korName3", "engName3", "categoryName3", null, 1L, false)
-		);
+	private final AlcoholQueryRepository alcoholQueryRepository;
 
-		return AlcoholSearchResponse.of(3L, searchDetails);
+	public PageResponse<?> searchAlcohols(AlcoholSearchRequest request, Long userId) {
+
+		AlcoholSearchCriteria criteria = AlcoholSearchCriteria.of(request, userId);
+
+		return alcoholQueryRepository.searchAlcohols(criteria);
 	}
 }
