9b1c4634457dc2b0db367e1530b1787b9360555d
whale0928 <rlagusrl928@gmail.com>
2024-05-04 16:18:07 +0900

    fix: 스프링시큐리티 경로 허용

--- src/main/java/app/bottlenote/global/security/SecurityConfig.java
+++ src/main/java/app/bottlenote/global/security/SecurityConfig.java
@@ -2,8 +2,6 @@ package app.bottlenote.global.security;
 
 import app.bottlenote.common.jwt.JwtAuthenticationFilter;
 import app.bottlenote.common.jwt.JwtTokenProvider;
-import java.util.Arrays;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,12 +10,16 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @Configuration
 @EnableWebSecurity
@@ -33,18 +35,18 @@ public class SecurityConfig {
 			.cors(cors -> cors.configurationSource(corsConfigurationSource()))
 			.csrf(AbstractHttpConfigurer::disable)
 			.authenticationProvider(socialLoginAuthenticationProvider)
-			.sessionManagement(sessionConfig ->
-				sessionConfig.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+			.sessionManagement(sessionConfig -> sessionConfig.sessionCreationPolicy(STATELESS))
 			.formLogin(AbstractHttpConfigurer::disable)
-			.authorizeHttpRequests(auth -> auth.requestMatchers("/api/v1/oauth/**")
-				.permitAll()  // 모든 사용자에게 접근 허용
-				.anyRequest().authenticated())
+			.authorizeHttpRequests(auth -> auth
+				.requestMatchers("/api/v1/oauth/**").permitAll()
+				.requestMatchers("/system/**").permitAll()
+				.anyRequest().permitAll()
+			)
 			.addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider),
 				UsernamePasswordAuthenticationFilter.class);
 		return http.build();
 	}
 
-	//  CORS 설정
 	@Bean
 	CorsConfigurationSource corsConfigurationSource() {
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
