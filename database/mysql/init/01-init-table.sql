78f921c02f97a66541bb2f7090e26896d68f8a70
whale0928 <rlagusrl928@gmail.com>
2024-05-07 21:39:37 +0900

    docs: data jpa test setup and tagging

--- database/mysql/init/01-init-table.sql
+++ database/mysql/init/01-init-table.sql
@@ -121,7 +121,7 @@ CREATE TABLE `rating`
     `create_by`      varchar(255) NULL COMMENT '최초 생성자',
     `last_modify_at` timestamp    NULL COMMENT '최종 생성일',
     `last_modify_by` varchar(255) NULL COMMENT '최종 생성자',
-    PRIMARY KEY (`alcohol_id`, `user_id`),
+    PRIMARY KEY (`alcohol_id`, `user_id`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4
   COLLATE = utf8mb4_unicode_ci COMMENT = '술 평점';
