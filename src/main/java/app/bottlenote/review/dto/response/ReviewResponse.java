4c69c94415fae37136a5d6622ddc0ffc259aaf57
chadongmin <cdm2883@naver.comn>
2024-05-09 01:54:52 +0900

    feat : ReviewResponse 생성

---
+++ src/main/java/app/bottlenote/review/dto/response/ReviewResponse.java
@@ -0,0 +1,62 @@
+package app.bottlenote.review.dto.response;
+
+import app.bottlenote.review.domain.constant.SizeType;
+import java.math.BigDecimal;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+
+@Getter
+@EqualsAndHashCode
+public class ReviewResponse {
+
+	private Long reviewId;
+	private BigDecimal price;
+	private SizeType sizeType;
+	private Long likeCount;
+	private Long replyCount;
+
+	private Long userId;
+	private String userNickname;
+	private String userProfileImage;
+
+	private Boolean myCommentYn;
+	private String status;
+	private Boolean myLikeYn;
+	private Boolean myReplyYn;
+
+
+	private String thumbnailImage;
+	private String createdAt;
+
+
+	@Builder
+	public ReviewResponse(Long reviewId, BigDecimal price, SizeType sizeType, Long likeCount,
+		Long replyCount,
+		Long userId, String userNickname, String userProfileImage, Boolean myCommentYn,
+		String status,
+		Boolean myLikeYn, Boolean myReplyYn, String thumbnailImage, String createdAt) {
+		this.reviewId = reviewId;
+		this.price = price;
+		this.sizeType = sizeType;
+		this.likeCount = likeCount;
+		this.replyCount = replyCount;
+		this.userId = userId;
+		this.userNickname = userNickname;
+		this.userProfileImage = userProfileImage;
+		this.myCommentYn = myCommentYn;
+		this.status = status;
+		this.myLikeYn = myLikeYn;
+		this.myReplyYn = myReplyYn;
+		this.thumbnailImage = thumbnailImage;
+		this.createdAt = createdAt;
+	}
+
+	public void setLikeCount(Long likeCount) {
+		this.likeCount = likeCount;
+	}
+
+	//DTO Injection을 위한 기본생성자
+	public ReviewResponse() {
+	}
+}
