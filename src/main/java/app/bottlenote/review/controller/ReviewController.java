b54c86ccfaebccffb33c2a1a202b4201456480af
chadongmin <cdm2883@naver.comn>
2024-05-10 00:49:32 +0900

    feat : 페이지네이션 적용에 따른 컨트롤러 수정

--- src/main/java/app/bottlenote/review/controller/ReviewController.java
+++ src/main/java/app/bottlenote/review/controller/ReviewController.java
@@ -3,11 +3,17 @@ package app.bottlenote.review.controller;
 import static app.bottlenote.global.data.response.GlobalResponse.success;
 
 import app.bottlenote.global.data.response.GlobalResponse;
+import app.bottlenote.global.security.SecurityUtil;
+import app.bottlenote.global.service.cursor.PageResponse;
+import app.bottlenote.global.service.meta.MetaService;
+import app.bottlenote.review.dto.request.PageableRequest;
+import app.bottlenote.review.dto.response.ReviewResponse;
 import app.bottlenote.review.service.ReviewService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,9 +27,24 @@ public class ReviewController {
 	private final ReviewService reviewService;
 
 	@GetMapping("/{alcoholId}")
-	public ResponseEntity<GlobalResponse> getReviews(@PathVariable Long alcoholId) {
+	public ResponseEntity<GlobalResponse> getReviews(
+		@PathVariable Long alcoholId,
+		@ModelAttribute PageableRequest pageableRequest) {
+
+		Long currentUserId = SecurityUtil.getCurrentUserId();
+
+		log.info("curruntUserId is : {}", currentUserId);
+		log.info("Pageable INFO  : {}", pageableRequest.toString());
+
+		PageResponse<ReviewResponse> pageResponse = reviewService.getReviews(alcoholId,
+			pageableRequest,
+			currentUserId);
+
 		return ResponseEntity.ok(success(
-			reviewService.getReviews(alcoholId)
+
+			pageResponse.content(),
+			MetaService.createMetaInfo()
+				.add("pageable", pageResponse.cursorPageable())
 		));
 	}
 
