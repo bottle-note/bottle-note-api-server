fc85f1185494ef03ce40e3832c71863c3c7fca05
whale0928 <rlagusrl928@gmail.com>
2024-05-09 21:59:53 +0900

    feat: implement friends mock info setup

--- src/main/java/app/bottlenote/alcohols/dto/response/detail/AlcoholDetailInfo.java
+++ src/main/java/app/bottlenote/alcohols/dto/response/detail/AlcoholDetailInfo.java
@@ -8,26 +8,21 @@ import java.util.List;
 @Getter
 @AllArgsConstructor
 public class AlcoholDetailInfo {
-	List<String> tags; // class java.util.List
-	private Long alcoholId;   // class java.lang.Long,
-	private String koreanCategory;   // class java.lang.String,
-	private String englishCategory;   // class java.lang.String,
-	private String thumbnailImage;   // class java.lang.String,
-	private String koreanName;   // class java.lang.String,
-	private String englishName;   // class java.lang.String,
-	private String koreanRegion;   // class java.lang.String,
-	private String englishRegion;   // class java.lang.String,
-	private String englishCask;   // class java.lang.String,
-	private String alcoholContent;   // % 기호 제거해야함.
-	private String koreanDistillery;   // class java.lang.String,
-	private String englishDistillery;   // class java.lang.String,
-	private Double averageRating;   // class java.lang.Double,
-	private Long totalRatings;   // class java.lang.Long,
-	private Double myRating;   // class java.lang.Double,
-	private Boolean isPicked;   // class java.lang.Boolean
-
-	 public static class Tag{
-		private String tag;
-
-	 }
+	private Long alcoholId;
+	private String alcoholUrlImg;
+	private String korName;
+	private String engName;
+	private String korCategory;
+	private String engCategory;
+	private String korRegion;
+	private String engRegion;
+	private String cask;
+	private String avg;
+	private String korDistillery;
+	private String engDistillery;
+	private Double rating;
+	private Long totalRatings;
+	private Double myRating;
+	private Boolean isPicked;
+	private List<String> tags;
 }
