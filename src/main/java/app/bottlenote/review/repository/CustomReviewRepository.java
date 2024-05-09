819dbb7316dccc2b161abafc457a48c5a34cec88
chadongmin <cdm2883@naver.comn>
2024-05-10 00:49:04 +0900

    feat : Pagination 구현

--- src/main/java/app/bottlenote/review/repository/CustomReviewRepository.java
+++ src/main/java/app/bottlenote/review/repository/CustomReviewRepository.java
@@ -1,10 +1,11 @@
 package app.bottlenote.review.repository;
 
+import app.bottlenote.global.service.cursor.CursorPageable;
+import app.bottlenote.global.service.cursor.PageResponse;
 import app.bottlenote.review.dto.response.ReviewResponse;
-import java.util.List;
 
 public interface CustomReviewRepository {
 
-	//TODO : 리턴타입 PageResponse로 수정예정
-	List<ReviewResponse> getReviews(Long alcoholId);
+	PageResponse<ReviewResponse> getReviews(Long alcoholId, CursorPageable cursorPageable,
+		Long userId);
 }
--- src/main/java/app/bottlenote/review/repository/CustomReviewRepositoryImpl.java
+++ src/main/java/app/bottlenote/review/repository/CustomReviewRepositoryImpl.java
@@ -5,6 +5,9 @@ import static app.bottlenote.like.domain.QLikes.likes;
 import static app.bottlenote.review.domain.QReview.review;
 import static app.bottlenote.review.domain.QReviewReply.reviewReply;
 
+import app.bottlenote.global.service.cursor.CursorPageable;
+import app.bottlenote.global.service.cursor.PageResponse;
+import app.bottlenote.review.dto.response.ReviewDetail;
 import app.bottlenote.review.dto.response.ReviewResponse;
 import com.querydsl.core.types.ExpressionUtils;
 import com.querydsl.core.types.Projections;
@@ -12,18 +15,21 @@ import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @RequiredArgsConstructor
 public class CustomReviewRepositoryImpl implements CustomReviewRepository {
 
 	private final JPAQueryFactory queryFactory;
 
 	@Override
-	public List<ReviewResponse> getReviews(Long alcoholId) {
+	public PageResponse<ReviewResponse> getReviews(Long alcoholId, CursorPageable pageable,
+		Long userId) {
 
-		return queryFactory
+		List<ReviewDetail> fetch = queryFactory
 			.select(Projections.fields(
-				ReviewResponse.class,
+				ReviewDetail.class,
 				review.id.as("reviewId"),
 				review.content.as("reviewContent"),
 				review.price.as("price"),
@@ -53,6 +59,45 @@ public class CustomReviewRepositoryImpl implements CustomReviewRepository {
 			.leftJoin(alcohol).on(alcohol.id.eq(review.alcohol.id))
 			.where(alcohol.id.eq(alcoholId))
 			.orderBy(review.createAt.asc())
+			.offset(pageable.getCursor())
+			.limit(pageable.getPageSize() + 1)
 			.fetch();
+
+		Long totalCount = queryFactory
+			.select(review.id.count())
+			.from(review)
+			.where(review.alcohol.id.eq(alcoholId))
+			.fetchOne();
+
+		CursorPageable cursorPageable = getCursorPageable(pageable, fetch);
+
+		log.info("CURSOR Pageable info :{}", cursorPageable.toString());
+		return PageResponse.of(ReviewResponse.of(totalCount, fetch), cursorPageable);
 	}
+
+
+	private CursorPageable getCursorPageable(CursorPageable pageable, List<ReviewDetail> fetch) {
+
+		boolean hasNext = isHasNext(pageable, fetch);
+		return CursorPageable.builder()
+			.cursor(pageable.getCursor() + pageable.getPageSize())
+			.pageSize(pageable.getPageSize())
+			.hasNext(hasNext)
+			.currentCursor(pageable.getCursor())
+			.build();
+	}
+
+	/**
+	 * 다음 페이지가 있는지 확인하는 메소드
+	 */
+	private boolean isHasNext(CursorPageable pageable, List<ReviewDetail> fetch) {
+		boolean hasNext = fetch.size() > pageable.getPageSize();
+
+		if (hasNext) {
+			fetch.remove(fetch.size() - 1);  // Remove the extra record
+		}
+		return hasNext;
+	}
+
+	//TODO :: ORDER SPECIFIER 구현예정
 }
