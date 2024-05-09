b45dfe0dee254875ea68c7638f301bd4cc4d3361
juye <kjy_1615@naver.com>
2024-05-09 14:25:09 +0900

    feat : 닉네임변경 코드 작성중..업데이트 안되는 이슈발생

--- src/main/java/app/bottlenote/user/controller/UserCommandController.java
+++ src/main/java/app/bottlenote/user/controller/UserCommandController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/api/v1//users")
+@RequestMapping("/api/v1/users")
 public class UserCommandController {
 
 	private final UserCommandService userCommandService;
