4133bb2913383509c6496af058217c6a7ed0f236
whale0928 <rlagusrl928@gmail.com>
2024-05-09 10:18:33 +0900

    feat: create Custom repo and service

---
+++ src/main/java/app/bottlenote/review/repository/CustomReviewQueryRepository.java
@@ -0,0 +1,13 @@
+package app.bottlenote.review.repository;
+
+
+import app.bottlenote.alcohols.dto.response.AlcoholDetail;
+
+import java.util.List;
+
+public interface CustomReviewQueryRepository {
+
+	List<AlcoholDetail.ReviewOfAlcoholDetail> findBestReviewsForAlcoholDetail(Long alcoholId, Long userId);
+
+	List<AlcoholDetail.ReviewOfAlcoholDetail> findReviewsForAlcoholDetail(Long alcoholId, Long userId);
+}
---
+++ src/main/java/app/bottlenote/review/repository/ReviewQueryRepository.java
@@ -0,0 +1,11 @@
+package app.bottlenote.review.repository;
+
+import app.bottlenote.review.domain.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewQueryRepository extends JpaRepository<Review, Long>, CustomReviewQueryRepository {
+
+
+}
