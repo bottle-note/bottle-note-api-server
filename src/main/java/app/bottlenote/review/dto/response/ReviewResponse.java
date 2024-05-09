a52a063d3a439a11f9a09fe47d04c7988191d963
chadongmin <cdm2883@naver.comn>
2024-05-10 00:46:58 +0900

    fix : 페이지네이션 적용을 위해 클래스 구조 수정 (totalCount + 리뷰 응답 response dto)

--- src/main/java/app/bottlenote/review/dto/response/ReviewResponse.java
+++ src/main/java/app/bottlenote/review/dto/response/ReviewResponse.java
@@ -1,59 +1,14 @@
 package app.bottlenote.review.dto.response;
 
-import app.bottlenote.review.domain.constant.SizeType;
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
-import lombok.Builder;
-import lombok.EqualsAndHashCode;
+import java.util.List;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
-@EqualsAndHashCode
+@AllArgsConstructor(staticName = "of")
 public class ReviewResponse {
 
-	private Long reviewId;
-	private String reviewContent;
-	private BigDecimal price;
-	private SizeType sizeType;
-	private Long likeCount;
-	private Long replyCount;
-	private String thumbnailImage;
-	private LocalDateTime reviewCreatedAt;
+	private final Long totalCount;
+	private final List<ReviewDetail> reviewList;
 
-	private Long userId;
-	private String userNickname;
-	private String userProfileImage;
-
-	//TODO: ReviewStatus Enum 타입으로 수정예정
-	private String status;
-
-	private Boolean isMyReview;
-	private Boolean isLikedByMe;
-	private Boolean hasCommentedByMe;
-
-	@Builder
-	public ReviewResponse(Long reviewId, String reviewContent, BigDecimal price, SizeType sizeType,
-		Long likeCount, Long replyCount, String thumbnailImage, LocalDateTime reviewCreatedAt,
-		Long userId, String userNickname, String userProfileImage, String status,
-		Boolean isMyReview,
-		Boolean isLikedByMe, Boolean hasCommentedByMe) {
-		this.reviewId = reviewId;
-		this.reviewContent = reviewContent;
-		this.price = price;
-		this.sizeType = sizeType;
-		this.likeCount = likeCount;
-		this.replyCount = replyCount;
-		this.thumbnailImage = thumbnailImage;
-		this.reviewCreatedAt = reviewCreatedAt;
-		this.userId = userId;
-		this.userNickname = userNickname;
-		this.userProfileImage = userProfileImage;
-		this.status = status;
-		this.isMyReview = isMyReview;
-		this.isLikedByMe = isLikedByMe;
-		this.hasCommentedByMe = hasCommentedByMe;
-	}
-
-	public ReviewResponse() {
-	}
 }
