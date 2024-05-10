13ed2e67d7b18fdc42a879efec1ee4395f840ae0
chadongmin <cdm2883@naver.comn>
2024-05-10 20:14:19 +0900

    fix : 매개변수 수정

--- src/main/java/app/bottlenote/review/repository/CustomReviewRepository.java
+++ src/main/java/app/bottlenote/review/repository/CustomReviewRepository.java
@@ -1,11 +1,11 @@
 package app.bottlenote.review.repository;
 
-import app.bottlenote.global.service.cursor.CursorPageable;
 import app.bottlenote.global.service.cursor.PageResponse;
+import app.bottlenote.review.dto.request.PageableRequest;
 import app.bottlenote.review.dto.response.ReviewResponse;
 
 public interface CustomReviewRepository {
 
-	PageResponse<ReviewResponse> getReviews(Long alcoholId, CursorPageable cursorPageable,
+	PageResponse<ReviewResponse> getReviews(Long alcoholId, PageableRequest pageableRequest,
 		Long userId);
 }
