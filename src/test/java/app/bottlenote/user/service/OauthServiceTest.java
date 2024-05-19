318c7f318be5f80ea36e6f848419c8a1d1c01162
chadongmin <cdm2883@naver.comn>
2024-05-19 23:54:55 +0900

    test : 리프레시 토큰 만료 시나리오 별 토큰 검증 테스트 케이스 추가

--- src/test/java/app/bottlenote/user/service/OauthServiceTest.java
+++ src/test/java/app/bottlenote/user/service/OauthServiceTest.java
@@ -1,6 +1,7 @@
 package app.bottlenote.user.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
@@ -23,6 +24,10 @@ import app.bottlenote.user.dto.request.OauthRequest;
 import app.bottlenote.user.dto.response.TokenDto;
 import app.bottlenote.user.exception.UserException;
 import app.bottlenote.user.repository.OauthRepository;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import java.util.Date;
 import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -221,5 +226,30 @@ class OauthServiceTest {
 		}
 	}
 
+	@Test
+	@DisplayName("리프레시 토큰이 만료되었을떄, 토큰 검증 메서드는 false를 반환한다.")
+	void test_token_validator_when_refresh_token_is_expired() {
 
+		Date now = new Date();
+
+		String tmpRefreshToken = Jwts.builder()
+			.setClaims(Jwts.claims().setSubject("cha"))
+			.setIssuedAt(now)
+			.setExpiration(new Date(now.getTime() - 1000))
+			.signWith(Keys.hmacShaKeyFor(
+					"c2VjdXJasdfasdgagasgasgIzNDU2Nzg5MGFiY2RlZmdoaWprbG1ub3BxcnN0dXZ3eHl6QWJDRGVGR2hJSktMTU5asfdasgdsldYWVphYmNkZWZnaGlrSg==".getBytes()),
+				SignatureAlgorithm.HS512)
+			.compact();
+
+		boolean isValid = JwtTokenValidator.validateToken(tmpRefreshToken);
+		assertFalse(isValid);
+	}
+
+	@Test
+	@DisplayName("리프레시 토큰이 만료되었을떄, 토큰 검증 메서드는 false를 반환한다.")
+	void test_token_validator_when_refresh_token_is_null() {
+
+		boolean isValid = JwtTokenValidator.validateToken(null);
+		assertFalse(isValid);
+	}
 }
