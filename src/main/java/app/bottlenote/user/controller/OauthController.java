256eb1390538abb3ce10a09166e142c39faee6e9
chadongmin <cdm2883@naver.comn>
2024-05-19 19:10:57 +0900

    refactor : 서비스 로직에서 반환한 TokenDto에서 리프레쉬 토큰 파싱하여 쿠키에 담는 로직 추가

--- src/main/java/app/bottlenote/user/controller/OauthController.java
+++ src/main/java/app/bottlenote/user/controller/OauthController.java
@@ -2,8 +2,12 @@ package app.bottlenote.user.controller;
 
 import app.bottlenote.global.data.response.GlobalResponse;
 import app.bottlenote.user.dto.request.OauthRequest;
-import app.bottlenote.user.dto.request.TokenRequest;
+import app.bottlenote.user.dto.response.OauthResponse;
+import app.bottlenote.user.dto.response.TokenDto;
 import app.bottlenote.user.service.OauthService;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -23,19 +27,34 @@ import org.springframework.web.bind.annotation.RestController;
 public class OauthController {
 
 	private final OauthService oauthService;
+	private static final String REFRESH_TOKEN_HEADER_PREFIX = "refresh-token";
 
 	@PostMapping("/login")
-	public ResponseEntity<GlobalResponse> oauthLogin(@RequestBody @Valid OauthRequest oauthReq) {
-		log.info("Received oauth request: {}", oauthReq);
+	public ResponseEntity<GlobalResponse> oauthLogin(@RequestBody @Valid OauthRequest oauthReq,
+		HttpServletResponse response) {
+
+		TokenDto token = oauthService.oauthLogin(oauthReq);
+
+		setRefreshTokenInCookie(response, token.getRefreshToken());
+
 		return ResponseEntity.ok(
-			GlobalResponse.success(oauthService.oauthLogin(oauthReq)));
+			GlobalResponse.success(OauthResponse.of(token.getAccessToken())));
 	}
 
 	@PostMapping("/reissue")
 	public ResponseEntity<GlobalResponse> oauthReissue(
-		@RequestBody @Valid TokenRequest tokenRequest) {
+		HttpServletRequest request,
+		HttpServletResponse response) {
+
+		String refreshToken = request.getHeader(REFRESH_TOKEN_HEADER_PREFIX);
+		log.info("refresh token in request header : {}", refreshToken);
+
+		TokenDto token = oauthService.refresh(refreshToken);
+
+		setRefreshTokenInCookie(response, token.getRefreshToken());
+
 		return ResponseEntity.ok(
-			GlobalResponse.success(oauthService.refresh(tokenRequest))
+			GlobalResponse.success(OauthResponse.of(token.getAccessToken()))
 		);
 	}
 
@@ -45,6 +64,15 @@ public class OauthController {
 		return ResponseEntity.ok(
 			GlobalResponse.success(oauthService.getCurrentUser())
 		);
+	}
+
+	private void setRefreshTokenInCookie(HttpServletResponse response, String refreshToken) {
+		Cookie cookie = new Cookie(REFRESH_TOKEN_HEADER_PREFIX, refreshToken);
+		cookie.setHttpOnly(true);
+		cookie.setSecure(true); // HTTPS인 경우에만 전송, 개발 환경에서는 필요에 따라 설정
+		cookie.setPath("/");
+		cookie.setMaxAge(7 * 24 * 60 * 60); // 7일 동안 유효
+		response.addCookie(cookie);
 	}
 
 
