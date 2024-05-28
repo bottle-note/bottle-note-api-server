6a93e7e4ba3ed68d7a2e3b5582ba3c43fbb676fd
chadongmin <cdm2883@naver.comn>
2024-05-28 20:46:09 +0900

    feat : ReviewController 구현

--- src/main/java/app/bottlenote/review/controller/ReviewController.java
+++ src/main/java/app/bottlenote/review/controller/ReviewController.java
@@ -1,10 +1,14 @@
 package app.bottlenote.review.controller;
 
+import static app.bottlenote.global.data.response.GlobalResponse.success;
+
 import app.bottlenote.global.data.response.GlobalResponse;
 import app.bottlenote.global.security.SecurityContextUtil;
 import app.bottlenote.global.service.cursor.PageResponse;
 import app.bottlenote.global.service.meta.MetaService;
 import app.bottlenote.review.dto.request.PageableRequest;
+import app.bottlenote.review.dto.request.ReviewCreateRequest;
+import app.bottlenote.review.dto.response.ReviewCreateResponse;
 import app.bottlenote.review.dto.response.ReviewResponse;
 import app.bottlenote.review.service.ReviewService;
 import lombok.RequiredArgsConstructor;
@@ -13,10 +17,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import static app.bottlenote.global.data.response.GlobalResponse.success;
 
 @Slf4j
 @RestController
@@ -26,6 +30,17 @@ public class ReviewController {
 
 	private final ReviewService reviewService;
 
+	@PostMapping
+	public ResponseEntity<GlobalResponse> createReviews(
+		@RequestBody ReviewCreateRequest reviewCreateRequest
+	) {
+		Long currentUserId = SecurityContextUtil.getUserIdByContext().orElse(null);
+
+		ReviewCreateResponse createdReview = reviewService.createReviews(reviewCreateRequest,
+			currentUserId);
+		return ResponseEntity.ok(success(createdReview));
+	}
+
 	@GetMapping("/{alcoholId}")
 	public ResponseEntity<GlobalResponse> getReviews(
 		@PathVariable Long alcoholId,
@@ -34,7 +49,8 @@ public class ReviewController {
 
 		Long currentUserId = SecurityContextUtil.getUserIdByContext().orElse(null);
 
-		log.info("currentUserId is : {} \nPageable INFO  : {}", currentUserId, pageableRequest.toString());
+		log.info("currentUserId is : {} \nPageable INFO  : {}", currentUserId,
+			pageableRequest.toString());
 
 		PageResponse<ReviewResponse> pageResponse = reviewService.getReviews(
 			alcoholId,
