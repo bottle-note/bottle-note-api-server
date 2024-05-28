b05e927cf24a28a1a3b415c5246e699027aa2e60
chadongmin <cdm2883@naver.comn>
2024-05-28 21:26:27 +0900

    feat : findAlcoholById 메서드 추가

--- src/main/java/app/bottlenote/alcohols/repository/AlcoholQueryRepository.java
+++ src/main/java/app/bottlenote/alcohols/repository/AlcoholQueryRepository.java
@@ -1,7 +1,11 @@
 package app.bottlenote.alcohols.repository;
 
 import app.bottlenote.alcohols.domain.Alcohol;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AlcoholQueryRepository extends JpaRepository<Alcohol, Long>, CustomAlcoholQueryRepository {
+public interface AlcoholQueryRepository extends JpaRepository<Alcohol, Long>,
+	CustomAlcoholQueryRepository {
+
+	Optional<Alcohol> findAlcoholById(Long id);
 }
