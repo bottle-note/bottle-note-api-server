2ad67152b2bb64c7b880f3633ca17891a41b29b8
chadongmin <cdm2883@naver.comn>
2024-05-28 20:47:04 +0900

    feat : ReviewCreateRequest 구현 (validation 미적용)

---
+++ src/main/java/app/bottlenote/review/dto/request/ReviewCreateRequest.java
@@ -0,0 +1,37 @@
+package app.bottlenote.review.dto.request;
+
+import app.bottlenote.alcohols.domain.TastingTag;
+import app.bottlenote.review.domain.constant.ReviewStatus;
+import app.bottlenote.review.domain.constant.SizeType;
+import jakarta.validation.constraints.NotNull;
+import java.math.BigDecimal;
+import java.util.List;
+
+public record ReviewCreateRequest(
+	@NotNull(message = "alcohol id는 Null일 수 없습니다.")
+	Long alcoholId,
+
+	Double rating,
+
+	ReviewStatus reviewStatus,
+
+	String content,
+
+	SizeType sizeType,
+
+	BigDecimal price,
+
+	String zipCode,
+
+	String address,
+
+	String detailAddress,
+
+	String imageUrl,
+	
+	List<TastingTag> tastingTag
+
+) {
+
+
+}
