2faeccfaaa0fc4984c1de7a8756090defdb178a3
chadongmin <cdm2883@naver.comn>
2024-06-01 23:05:04 +0900

    fix : ReviewCreateResponse 에서 content와 전체 리뷰 조회 콜백 URL 반환하도록 수정

--- src/main/java/app/bottlenote/review/dto/response/ReviewCreateResponse.java
+++ src/main/java/app/bottlenote/review/dto/response/ReviewCreateResponse.java
@@ -1,13 +1,30 @@
 package app.bottlenote.review.dto.response;
 
+import static app.bottlenote.review.exception.ReviewExceptionCode.INVALID_CALL_BACK_URL;
+
+import app.bottlenote.review.exception.ReviewException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
 @Getter
+@EqualsAndHashCode
 public class ReviewCreateResponse {
 
 	private final Long id;
+	private final String content;
+	private final URL callback;
 
-	public ReviewCreateResponse(Long id) {
+	@Builder
+	public ReviewCreateResponse(Long id, String content, String callback) {
 		this.id = id;
+		this.content = content;
+		try {
+			this.callback = new URL("https://bottle-note.com/api/v1/reviews/" + callback);
+		} catch (MalformedURLException e) {
+			throw new ReviewException(INVALID_CALL_BACK_URL);
+		}
 	}
 }
