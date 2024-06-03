f7fb26592e95d536c287893a6d7d33593c9ea076
chadongmin <cdm2883@naver.comn>
2024-06-04 00:22:32 +0900

    fix  :오타수정

--- src/main/java/app/bottlenote/review/exception/ReviewException.java
+++ src/main/java/app/bottlenote/review/exception/ReviewException.java
@@ -1,13 +1,12 @@
 package app.bottlenote.review.exception;
 
 import app.bottlenote.global.exception.custom.AbstractCustomException;
-import app.bottlenote.global.exception.custom.code.ExceptionCode;
 import lombok.Getter;
 
 @Getter
 public class ReviewException extends AbstractCustomException {
 
-	public ReviewException(ExceptionCode code) {
+	public ReviewException(ReviewExceptionCode code) {
 		super(code);
 	}
 }
--- src/main/java/app/bottlenote/review/exception/ReviewExceptionCode.java
+++ src/main/java/app/bottlenote/review/exception/ReviewExceptionCode.java
@@ -7,7 +7,7 @@ public enum ReviewExceptionCode implements ExceptionCode {
 
 	INVALID_TASTING_TAG_LENGTH(HttpStatus.BAD_REQUEST, "테이스팅 태그의 길이는 12자 이하로만 작성할 수 있습니다."),
 	INVALID_TASTING_TAG_LIST_SIZE(HttpStatus.BAD_REQUEST, "테이스팅 태그는 10개까지만 작성할 수 있습니다."),
-	IVALID_IMAGE_URL_MAX_SIZE(HttpStatus.BAD_REQUEST, "이미지는 최대 5장까지만 업로드 할 수 있습니다"),
+	INVALID_IMAGE_URL_MAX_SIZE(HttpStatus.BAD_REQUEST, "이미지는 최대 5장까지만 업로드 할 수 있습니다"),
 	INVALID_CALL_BACK_URL(HttpStatus.BAD_REQUEST, "잘못된 콜백 URL입니다.");
 	private final HttpStatus httpStatus;
 	private final String message;
