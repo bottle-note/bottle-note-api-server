489b56adb98347f42d2f11ea923b54fdad308cae
chadongmin <cdm2883@naver.comn>
2024-06-09 17:31:22 +0900

    docs : docs 테스트코드 작성 및 restdocs 작성

--- src/test/java/app/bottlenote/docs/review/ReviewControllerDocsTest.java
+++ src/test/java/app/bottlenote/docs/review/ReviewControllerDocsTest.java
@@ -173,6 +173,75 @@ class ReviewControllerDocsTest extends AbstractRestDocs {
 			);
 	}
 
+	@Test
+	@DisplayName("내가 작성한 리뷰를 조회할 수 있다.")
+	void my_review_read_test() throws Exception {
+
+		try (MockedStatic<SecurityContextUtil> mockedValidator = mockStatic(
+			SecurityContextUtil.class)) {
+
+			mockedValidator.when(SecurityContextUtil::getUserIdByContext)
+				.thenReturn(Optional.of(1L));
+
+			//given
+			PageResponse<ReviewResponse> response = getMyReviewResponse();
+
+			//when
+			when(reviewService.getMyReview(any(), any(), any())).thenReturn(
+				response);
+
+			//then
+			mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/reviews/me/1")
+					.param("sortType", "POPULAR")
+					.param("sortOrder", "DESC")
+					.param("cursor", "0")
+					.param("pageSize", "2")
+				)
+				.andExpect(status().isOk())
+				.andDo(
+					document("review/my-review-read",
+						queryParameters(
+							parameterWithName("sortType").optional().description("정렬 타입(해당 문서 하단 enum 참조)"),
+							parameterWithName("sortOrder").optional().description("정렬 순서(해당 문서 하단 enum 참조)"),
+							parameterWithName("cursor").optional().description("조회 할 시작 기준 위치"),
+							parameterWithName("pageSize").optional().description("조회 할 페이지 사이즈")
+						),
+						responseFields(
+							fieldWithPath("success").description("응답 성공 여부"),
+							fieldWithPath("code").description("응답 코드(http status code)"),
+							fieldWithPath("data.totalCount").description("전체 리뷰 리스트의 크기"),
+							fieldWithPath("data.reviewList[].reviewId").description("리뷰 ID"),
+							fieldWithPath("data.reviewList[].reviewContent").description("리뷰 내용"),
+							fieldWithPath("data.reviewList[].price").description("가격"),
+							fieldWithPath("data.reviewList[].sizeType").description("사이즈 타입 (BOTTLE, GLASS)"),
+							fieldWithPath("data.reviewList[].likeCount").description("좋아요 개수"),
+							fieldWithPath("data.reviewList[].replyCount").description("댓글 개수"),
+							fieldWithPath("data.reviewList[].reviewImageUrl").description("리뷰 썸네일 이미지"),
+							fieldWithPath("data.reviewList[].createAt").description("리뷰 등록 일시"),
+							fieldWithPath("data.reviewList[].userId").description("유저 ID"),
+							fieldWithPath("data.reviewList[].nickName").description("유저 닉네임"),
+							fieldWithPath("data.reviewList[].userProfileImage").description("유저 프로필 이미지"),
+							fieldWithPath("data.reviewList[].rating").description("리뷰에 등록된 별점"),
+							fieldWithPath("data.reviewList[].status").description("리뷰 공개 상태 (PUBLIC, PRIVATE)"),
+							fieldWithPath("data.reviewList[].isMyReview").description("내가 작성한 리뷰인지 여부"),
+							fieldWithPath("data.reviewList[].isLikedByMe").description("내가 좋아요 누른 리뷰인지 여부"),
+							fieldWithPath("data.reviewList[].hasReplyByMe").description("내가 댓글을 단 리뷰인지 여부"),
+							fieldWithPath("errors").ignored(),
+							fieldWithPath("meta.serverEncoding").ignored(),
+							fieldWithPath("meta.serverVersion").ignored(),
+							fieldWithPath("meta.serverPathVersion").ignored(),
+							fieldWithPath("meta.serverResponseTime").ignored(),
+							fieldWithPath("meta.pageable").description("페이징 정보"),
+							fieldWithPath("meta.pageable.currentCursor").description("조회 시 기준 커서"),
+							fieldWithPath("meta.pageable.cursor").description("다음 페이지 커서"),
+							fieldWithPath("meta.pageable.pageSize").description("조회된 페이지 사이즈"),
+							fieldWithPath("meta.pageable.hasNext").description("다음 페이지 존재 여부")
+						)
+					)
+				);
+		}
+	}
+
 	private ReviewCreateRequest getReviewCreateRequest() {
 		return new ReviewCreateRequest(
 			1L,
@@ -232,6 +301,58 @@ class ReviewControllerDocsTest extends AbstractRestDocs {
 			.reviewImageUrl("https://picsum.photos/600/600")
 			.createAt(LocalDateTime.now().minusDays(1))
 			.userId(2L)
+			.nickName("test_user_2")
+			.userProfileImage("user_profile_image_2")
+			.rating(4.0)
+			.status(ReviewStatus.PUBLIC)
+			.isMyReview(true)
+			.isLikedByMe(true)
+			.hasReplyByMe(false)
+			.build();
+
+		Long totalCount = 2L;
+		List<ReviewDetail> reviewDetails = List.of(reviewDetail_1, reviewDetail_2);
+		CursorPageable cursorPageable = CursorPageable.builder()
+			.currentCursor(0L)
+			.cursor(1L)
+			.pageSize(2L)
+			.hasNext(false)
+			.build();
+
+		ReviewResponse response = ReviewResponse.of(totalCount, reviewDetails);
+		return PageResponse.of(response, cursorPageable);
+	}
+
+	private PageResponse<ReviewResponse> getMyReviewResponse() {
+		ReviewDetail reviewDetail_1 = ReviewDetail.builder()
+			.reviewId(1L)
+			.reviewContent("맛있습니다")
+			.price(BigDecimal.valueOf(100000L))
+			.sizeType(SizeType.BOTTLE)
+			.likeCount(5L)
+			.replyCount(3L)
+			.reviewImageUrl("https://picsum.photos/600/600")
+			.createAt(LocalDateTime.now())
+			.userId(1L)
+			.nickName("test_user_1")
+			.userProfileImage("user_profile_image_1")
+			.rating(4.0)
+			.status(ReviewStatus.PUBLIC)
+			.isMyReview(true)
+			.isLikedByMe(true)
+			.hasReplyByMe(false)
+			.build();
+
+		ReviewDetail reviewDetail_2 = ReviewDetail.builder()
+			.reviewId(2L)
+			.reviewContent("이 가게에서 완전 가성비로 먹었어요")
+			.price(BigDecimal.valueOf(50000L))
+			.sizeType(SizeType.BOTTLE)
+			.likeCount(3L)
+			.replyCount(6L)
+			.reviewImageUrl("https://picsum.photos/600/600")
+			.createAt(LocalDateTime.now().minusDays(1))
+			.userId(1L)
 			.nickName("test_user_2")
 			.userProfileImage("user_profile_image_2")
 			.rating(4.0)
