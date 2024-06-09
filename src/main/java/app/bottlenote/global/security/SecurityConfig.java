b8f12bcea45a90ad627a005c30e1ae8a7fe1e0ca
chadongmin <cdm2883@naver.comn>
2024-06-09 15:34:21 +0900

    feat : SecurityConfig에 authenticated 추가

--- src/main/java/app/bottlenote/global/security/SecurityConfig.java
+++ src/main/java/app/bottlenote/global/security/SecurityConfig.java
@@ -10,6 +10,7 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -55,7 +56,8 @@ public class SecurityConfig {
 				.requestMatchers("/api/v1/picks/**").authenticated()
 				.requestMatchers("/api/v1/s3/**").authenticated()
 				.requestMatchers("/api/v1/follow").authenticated()
-				.requestMatchers("/api/v1/reviews").authenticated()
+				.requestMatchers(HttpMethod.POST, "/api/v1/reviews").authenticated()
+				.requestMatchers("/api/v1/reviews/me/**").authenticated()
 				.anyRequest().permitAll()
 			)
 			.addFilterBefore(new JwtAuthenticationFilter(jwtAuthenticationManager), UsernamePasswordAuthenticationFilter.class)
