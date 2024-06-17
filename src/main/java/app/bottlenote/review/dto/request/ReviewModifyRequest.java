b06d10bf534cd47bc41c66797364b7b25556ee89
chadongmin <cdm2883@naver.comn>
2024-06-17 16:47:31 +0900

    feat : ReviewModifyRequest DTO 추가

---
+++ src/main/java/app/bottlenote/review/dto/request/ReviewModifyRequest.java
@@ -0,0 +1,28 @@
+package app.bottlenote.review.dto.request;
+
+import app.bottlenote.review.domain.constant.ReviewStatus;
+import app.bottlenote.review.domain.constant.SizeType;
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import java.math.BigDecimal;
+import java.util.List;
+
+
+public record ReviewModifyRequest(
+	@NotEmpty(message = "리뷰 내용을 입력해주세요")
+	@Size(max = 500)
+	String content,
+	ReviewStatus reviewStatus,
+
+	@DecimalMin(value = "0.0", message = "가격은 0 이상이어야 합니다.")
+	@DecimalMax(value = "1000000000000", message = "입력할 수 있는 가격의 범위가 아닙니다.")
+	BigDecimal price,
+	SizeType sizeType,
+	List<String> tastingTagList,
+
+	LocationInfo locationInfo
+
+) {
+}
