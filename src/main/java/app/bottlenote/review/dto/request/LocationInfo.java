142585be44e580c8d194de896ea62250e9028f29
chadongmin <cdm2883@naver.comn>
2024-06-21 14:24:03 +0900

    refactor : validation 수정

--- src/main/java/app/bottlenote/review/dto/request/LocationInfo.java
+++ src/main/java/app/bottlenote/review/dto/request/LocationInfo.java
@@ -1,10 +1,14 @@
 package app.bottlenote.review.dto.request;
 
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Builder;
 
 public record LocationInfo(
+	@NotEmpty
 	String zipCode,
+	@NotEmpty
 	String address,
+	@NotEmpty
 	String detailAddress) {
 
 	@Builder
--- src/main/java/app/bottlenote/review/dto/request/ReviewModifyRequest.java
+++ src/main/java/app/bottlenote/review/dto/request/ReviewModifyRequest.java
@@ -2,9 +2,11 @@ package app.bottlenote.review.dto.request;
 
 import app.bottlenote.review.domain.constant.ReviewStatus;
 import app.bottlenote.review.domain.constant.SizeType;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.DecimalMax;
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import java.math.BigDecimal;
 import java.util.List;
@@ -14,14 +16,23 @@ public record ReviewModifyRequest(
 	@NotEmpty(message = "리뷰 내용을 입력해주세요")
 	@Size(max = 500)
 	String content,
-	ReviewStatus reviewStatus,
+
+	@NotNull
+	ReviewStatus status,
 
 	@DecimalMin(value = "0.0", message = "가격은 0 이상이어야 합니다.")
 	@DecimalMax(value = "1000000000000", message = "입력할 수 있는 가격의 범위가 아닙니다.")
+	@NotNull
 	BigDecimal price,
+
+	@NotNull
 	SizeType sizeType,
+
+	@NotNull
 	List<String> tastingTagList,
 
+	@NotNull
+	@Valid
 	LocationInfo locationInfo
 
 ) {
