35ded21d2312d81c7765d530d33f78ebe94eeaf1
chadongmin <cdm2883@naver.comn>
2024-06-21 14:21:04 +0900

    refactor : 리뷰 수정 로직 수정

--- src/main/java/app/bottlenote/review/service/ReviewService.java
+++ src/main/java/app/bottlenote/review/service/ReviewService.java
@@ -2,7 +2,6 @@ package app.bottlenote.review.service;
 
 import static app.bottlenote.alcohols.exception.AlcoholExceptionCode.ALCOHOL_NOT_FOUND;
 import static app.bottlenote.review.exception.ReviewExceptionCode.INVALID_IMAGE_URL_MAX_SIZE;
-import static app.bottlenote.review.exception.ReviewExceptionCode.INVALID_TASTING_TAG_LIST_SIZE;
 import static app.bottlenote.review.exception.ReviewExceptionCode.REVIEW_NOT_FOUND;
 import static app.bottlenote.user.exception.UserExceptionCode.USER_NOT_FOUND;
 
@@ -12,13 +11,14 @@ import app.bottlenote.alcohols.exception.AlcoholException;
 import app.bottlenote.common.image.ImageUtil;
 import app.bottlenote.global.service.cursor.PageResponse;
 import app.bottlenote.review.domain.Review;
+import app.bottlenote.review.domain.ReviewDomainService;
 import app.bottlenote.review.domain.ReviewImage;
+import app.bottlenote.review.domain.ReviewModifyVO;
 import app.bottlenote.review.domain.ReviewTastingTag;
 import app.bottlenote.review.dto.request.PageableRequest;
 import app.bottlenote.review.dto.request.ReviewCreateRequest;
 import app.bottlenote.review.dto.request.ReviewModifyRequest;
 import app.bottlenote.review.dto.response.ReviewCreateResponse;
-import app.bottlenote.review.dto.response.ReviewDetail;
 import app.bottlenote.review.dto.response.ReviewResponse;
 import app.bottlenote.review.exception.ReviewException;
 import app.bottlenote.review.repository.ReviewImageRepository;
@@ -28,8 +28,8 @@ import app.bottlenote.user.domain.User;
 import app.bottlenote.user.exception.UserException;
 import app.bottlenote.user.repository.UserCommandRepository;
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
-import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -40,14 +40,17 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class ReviewService {
 
+	private final ReviewDomainService reviewDomainService;
+
 	private final ReviewRepository reviewRepository;
 	private final AlcoholQueryRepository alcoholQueryRepository;
 	private final UserCommandRepository userCommandRepository;
 	private final ReviewImageRepository reviewImageRepository;
 	private final ReviewTastingTagRepository reviewTastingTagRepository;
 
-	private static final int TASTING_TAG_MAX_SIZE = 10;
 	private static final int REVIEW_IMAGE_MAX_SIZE = 5;
+
+	public static final String REVIEW_MODIFY_RESPONSE = "리뷰 수정이 완료되었습니다";
 
 	@Transactional
 	public ReviewCreateResponse createReviews(ReviewCreateRequest reviewCreateRequest, Long currentUserId) {
@@ -94,19 +97,12 @@ public class ReviewService {
 
 			reviewImageRepository.saveAll(reviewImageList);
 		}
+		if (!Objects.isNull(reviewCreateRequest.tastingTagList())) {
 
-		Set<ReviewTastingTag> reviewTastingTags = reviewCreateRequest.tastingTagList().stream()
-			.map(tastingTag -> ReviewTastingTag.builder()
-				.review(review)
-				.tastingTag((tastingTag))
-				.build())
-			.collect(Collectors.toSet());
+			Set<ReviewTastingTag> reviewTastingTags = reviewDomainService.createValidatedReviewTastingTag(reviewCreateRequest.tastingTagList(), review);
 
-		if (!isValidReviewTastingTag(reviewTastingTags)) {
-			throw new ReviewException(INVALID_TASTING_TAG_LIST_SIZE);
+			reviewTastingTagRepository.saveAll(reviewTastingTags);
 		}
-
-		reviewTastingTagRepository.saveAll(reviewTastingTags);
 
 		return ReviewCreateResponse.builder()
 			.id(saveReview.getId())
@@ -118,11 +114,6 @@ public class ReviewService {
 	private boolean isValidReviewImageList(List<ReviewImage> reviewImageList) {
 		return reviewImageList.size() <= REVIEW_IMAGE_MAX_SIZE;
 	}
-
-	private boolean isValidReviewTastingTag(Set<ReviewTastingTag> reviewTastingTags) {
-		return reviewTastingTags.size() <= TASTING_TAG_MAX_SIZE;
-	}
-
 
 	@Transactional(readOnly = true)
 	public PageResponse<ReviewResponse> getReviews(
@@ -150,7 +141,7 @@ public class ReviewService {
 	}
 
 	@Transactional
-	public ReviewDetail modifyReviews(
+	public String modifyReviews(
 		ReviewModifyRequest reviewModifyRequest,
 		Long reviewId,
 		Long currentUserId) {
@@ -159,9 +150,32 @@ public class ReviewService {
 			() -> new ReviewException(REVIEW_NOT_FOUND)
 		);
 
-		log.info(reviewModifyRequest.toString());
-		review.changeReview(reviewModifyRequest);
+		ReviewModifyVO reviewModifyVO = reviewDomainService.createValidatedReview(reviewModifyRequest);
 
-		return reviewRepository.getReview(reviewId, currentUserId);
+		review.modifyReview(reviewModifyVO);
+
+		List<String> requestedTastingTag = reviewModifyRequest.tastingTagList();
+
+		updateReviewTastingTags(review, requestedTastingTag);
+
+		return REVIEW_MODIFY_RESPONSE;
+
+	}
+
+	private void updateReviewTastingTags(Review review, List<String> requestedTastingTag) {
+
+		if (requestedTastingTag.isEmpty()) {
+
+			reviewTastingTagRepository.deleteByReviewId(review.getId());
+
+		} else {
+
+			Set<ReviewTastingTag> reviewTastingTags = reviewDomainService.createValidatedReviewTastingTag(requestedTastingTag, review);
+
+			reviewTastingTagRepository.deleteByReviewId(review.getId());
+			reviewTastingTagRepository.saveAll(reviewTastingTags);
+
+			review.updateTastingTags(reviewTastingTags);
+		}
 	}
 }
