7860944da1c1439143d59a4183abc2a89a865b2e
chadongmin <cdm2883@naver.comn>
2024-06-22 22:05:00 +0900

    refactor : Validation 수정 (Not Null 어노테이션 추가)

--- src/main/java/app/bottlenote/review/dto/request/ReviewModifyRequest.java
+++ src/main/java/app/bottlenote/review/dto/request/ReviewModifyRequest.java
@@ -6,6 +6,7 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.DecimalMax;
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import java.math.BigDecimal;
 import java.util.List;
@@ -16,17 +17,25 @@ public record ReviewModifyRequest(
 	@Size(max = 500)
 	String content,
 
+	@NotNull
 	ReviewStatus status,
 
 	@DecimalMin(value = "0.0", message = "가격은 0 이상이어야 합니다.")
 	@DecimalMax(value = "1000000000000", message = "입력할 수 있는 가격의 범위가 아닙니다.")
+	@NotNull
 	BigDecimal price,
 
+	@NotNull
+	List<ReviewImageInfo> imageUrlList,
+
+	@NotNull
 	SizeType sizeType,
 
+	@NotNull
 	List<String> tastingTagList,
 
 	@Valid
+	@NotNull
 	LocationInfo locationInfo
 
 ) {
