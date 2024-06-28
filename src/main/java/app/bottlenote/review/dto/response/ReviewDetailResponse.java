48593b1b71a69cf1d0a887a6df840c6c3bb9a8c8
chadongmin <cdm2883@naver.comn>
2024-06-28 22:49:56 +0900

    feat : ReviewDetailResponse 클래스 추가

---
+++ src/main/java/app/bottlenote/review/dto/response/ReviewDetailResponse.java
@@ -0,0 +1,44 @@
+package app.bottlenote.review.dto.response;
+
+import app.bottlenote.review.domain.constant.ReviewDisplayStatus;
+import app.bottlenote.review.domain.constant.SizeType;
+import app.bottlenote.review.dto.request.ReviewImageInfo;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class ReviewDetailResponse extends ReviewDetail {
+
+	private AlcoholInfo alcoholInfo;
+
+	private List<ReviewImageInfo> reviewImageList;
+
+	private List<ReviewReplyInfo> reviewReplyList;
+
+
+	public ReviewDetailResponse(Long reviewId, String reviewContent, BigDecimal price, SizeType sizeType, Long likeCount, Long replyCount, String reviewImageUrl, LocalDateTime createAt, Long userId, String nickName, String userProfileImage,
+		Double rating, String zipCode, String address, String detailAddress, ReviewDisplayStatus status, Boolean isMyReview, Boolean isLikedByMe, Boolean hasReplyByMe, List<String> reviewTastingTag, AlcoholInfo alcoholInfo,
+		List<ReviewImageInfo> reviewImageList, List<ReviewReplyInfo> reviewReplyList) {
+		super(reviewId, reviewContent, price, sizeType, likeCount, replyCount, reviewImageUrl, createAt, userId, nickName, userProfileImage, rating, zipCode, address, detailAddress, status, isMyReview, isLikedByMe, hasReplyByMe,
+			reviewTastingTag);
+		this.alcoholInfo = alcoholInfo;
+		this.reviewImageList = reviewImageList;
+		this.reviewReplyList = reviewReplyList;
+	}
+
+	public void updateAlcoholInfo(AlcoholInfo alcoholInfo) {
+		this.alcoholInfo = alcoholInfo;
+	}
+
+	public void updateReviewImageList(List<ReviewImageInfo> reviewImageInfo) {
+		this.reviewImageList = reviewImageInfo;
+	}
+
+	public void updateReviewReplyList(List<ReviewReplyInfo> reviewReplyList) {
+		this.reviewReplyList = reviewReplyList;
+	}
+}
