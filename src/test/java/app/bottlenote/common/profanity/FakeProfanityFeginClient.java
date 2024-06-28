c2aaae50ec8d0d1b7c09eea747bae16ebe78283f
whale0928 <rlagusrl928@gmail.com>
2024-06-28 21:19:42 +0900

    fix:test infomation

--- src/test/java/app/bottlenote/common/profanity/FakeProfanityFeginClient.java
+++ src/test/java/app/bottlenote/common/profanity/FakeProfanityFeginClient.java
@@ -1,6 +1,7 @@
 package app.bottlenote.common.profanity;
 
 import app.bottlenote.common.profanity.fegin.ProfanityFeginClient;
+import app.bottlenote.common.profanity.request.ProfanityRequest;
 import app.bottlenote.common.profanity.response.Detected;
 import app.bottlenote.common.profanity.response.ProfanityResponse;
 import org.apache.logging.log4j.LogManager;
@@ -12,15 +13,9 @@ import java.util.List;
 public class FakeProfanityFeginClient implements ProfanityFeginClient {
 
 
-	private static final List<String> words = List.of("비속어", "욕설","개자식");
+	private static final List<String> words = List.of("비속어", "욕설", "개자식");
 	private static final String trackingId = "019054c5-dfb1-777d-b567-95688c87f135";
 	private static final Logger log = LogManager.getLogger(FakeProfanityFeginClient.class);
-
-	@Override
-	public ResponseEntity<ProfanityResponse> requestVerificationProfanity(String text) {
-		log.info("[Fake] requestVerificationProfanity 욕설 검사 요청 : {}", text);
-		return ResponseEntity.ok(createProfanityResponse(text));
-	}
 
 	private ProfanityResponse createProfanityResponse(final String text) {
 		log.info("[Fake] createProfanityResponse  욕설 검사 결과 : {}", text);
@@ -43,5 +38,11 @@ public class FakeProfanityFeginClient implements ProfanityFeginClient {
 			.filtered(filtered)
 			.elapsed("0.00005961 s / 0.05961 ms / 59.605 µs")
 			.build();
+	}
+
+	@Override
+	public ResponseEntity<ProfanityResponse> requestVerificationProfanity(ProfanityRequest request) {
+		log.info("[Fake] requestVerificationProfanity 욕설 검사 요청 : {}", request);
+		return ResponseEntity.ok(createProfanityResponse(request.text()));
 	}
 }
