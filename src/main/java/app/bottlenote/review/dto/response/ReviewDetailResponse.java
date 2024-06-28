1b0e0f6bc9fa9823cf9b638f409e9308ea9bfc9d
chadongmin <cdm2883@naver.comn>
2024-06-28 23:54:55 +0900

    refactor : ReviewDetailResponse 생성자 추가

--- src/main/java/app/bottlenote/review/dto/response/ReviewDetailResponse.java
+++ src/main/java/app/bottlenote/review/dto/response/ReviewDetailResponse.java
@@ -1,19 +1,14 @@
 package app.bottlenote.review.dto.response;
 
-import app.bottlenote.review.domain.constant.ReviewDisplayStatus;
-import app.bottlenote.review.domain.constant.SizeType;
 import app.bottlenote.review.dto.request.ReviewImageInfo;
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
 import java.util.List;
-import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
-@EqualsAndHashCode
+@EqualsAndHashCode(callSuper = true)
 public class ReviewDetailResponse extends ReviewResponse {
 
 	private AlcoholInfo alcoholInfo;
@@ -22,14 +17,12 @@ public class ReviewDetailResponse extends ReviewResponse {
 
 	private List<ReviewReplyInfo> reviewReplyList;
 
-	@Builder
-	public ReviewDetailResponse(Long reviewId, String reviewContent, BigDecimal price, SizeType sizeType, Long likeCount, Long replyCount, String reviewImageUrl, LocalDateTime createAt, Long userId, String nickName, String userProfileImage,
-		Double rating, String zipCode, String address, String detailAddress, ReviewDisplayStatus status, Boolean isMyReview, Boolean isLikedByMe, Boolean hasReplyByMe, AlcoholInfo alcoholInfo, List<ReviewImageInfo> reviewImageList,
-		List<ReviewReplyInfo> reviewReplyList) {
-		super(reviewId, reviewContent, price, sizeType, likeCount, replyCount, reviewImageUrl, createAt, userId, nickName, userProfileImage, rating, zipCode, address, detailAddress, status, isMyReview, isLikedByMe, hasReplyByMe);
-		this.alcoholInfo = alcoholInfo;
-		this.reviewImageList = reviewImageList;
-		this.reviewReplyList = reviewReplyList;
+	public ReviewDetailResponse(ReviewResponse reviewResponse) {
+		super(reviewResponse.getReviewId(), reviewResponse.getReviewContent(), reviewResponse.getPrice(), reviewResponse.getSizeType(),
+			reviewResponse.getLikeCount(), reviewResponse.getReplyCount(), reviewResponse.getReviewImageUrl(), reviewResponse.getCreateAt(),
+			reviewResponse.getUserId(), reviewResponse.getNickName(), reviewResponse.getUserProfileImage(), reviewResponse.getRating(),
+			reviewResponse.getZipCode(), reviewResponse.getAddress(), reviewResponse.getDetailAddress(), reviewResponse.getStatus(),
+			reviewResponse.getIsMyReview(), reviewResponse.getIsLikedByMe(), reviewResponse.getHasReplyByMe());
 	}
 
 	public void updateAlcoholInfo(AlcoholInfo alcoholInfo) {
