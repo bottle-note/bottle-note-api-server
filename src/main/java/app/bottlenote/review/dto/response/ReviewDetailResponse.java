97d2b454ded6c9973f639ae88a362afae7b92028
chadongmin <cdm2883@naver.comn>
2024-06-30 15:37:10 +0900

    refactor : record로 변경

--- src/main/java/app/bottlenote/review/dto/response/ReviewDetailResponse.java
+++ src/main/java/app/bottlenote/review/dto/response/ReviewDetailResponse.java
@@ -2,36 +2,20 @@ package app.bottlenote.review.dto.response;
 
 import app.bottlenote.review.dto.request.ReviewImageInfo;
 import java.util.List;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
 
-@Getter
-@EqualsAndHashCode(callSuper = true)
-public class ReviewDetailResponse extends ReviewResponse {
+public record ReviewDetailResponse(
+	AlcoholInfo alcoholInfo,
 
-	private AlcoholInfo alcoholInfo;
+	ReviewResponse reviewResponse,
 
-	private List<ReviewImageInfo> reviewImageList;
+	List<ReviewImageInfo> reviewImageList,
 
-	private List<ReviewReplyInfo> reviewReplyList;
+	List<ReviewReplyInfo> reviewReplyList
 
-	public ReviewDetailResponse(ReviewResponse reviewResponse) {
-		super(reviewResponse.getReviewId(), reviewResponse.getReviewContent(), reviewResponse.getPrice(), reviewResponse.getSizeType(),
-			reviewResponse.getLikeCount(), reviewResponse.getReplyCount(), reviewResponse.getReviewImageUrl(), reviewResponse.getCreateAt(),
-			reviewResponse.getUserId(), reviewResponse.getNickName(), reviewResponse.getUserProfileImage(), reviewResponse.getRating(),
-			reviewResponse.getZipCode(), reviewResponse.getAddress(), reviewResponse.getDetailAddress(), reviewResponse.getStatus(),
-			reviewResponse.getIsMyReview(), reviewResponse.getIsLikedByMe(), reviewResponse.getHasReplyByMe());
-	}
+) {
 
-	public void updateAlcoholInfo(AlcoholInfo alcoholInfo) {
-		this.alcoholInfo = alcoholInfo;
-	}
-
-	public void updateReviewImageList(List<ReviewImageInfo> reviewImageInfo) {
-		this.reviewImageList = reviewImageInfo;
-	}
-
-	public void updateReviewReplyList(List<ReviewReplyInfo> reviewReplyList) {
-		this.reviewReplyList = reviewReplyList;
+	public static ReviewDetailResponse create(AlcoholInfo alcoholInfo, ReviewResponse reviewResponse, List<ReviewImageInfo> reviewImageList, List<ReviewReplyInfo> reviewReplyList) {
+		return new ReviewDetailResponse(alcoholInfo, reviewResponse, reviewImageList, reviewReplyList);
 	}
 }
+
