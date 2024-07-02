aade43eb1ca14177e911a201405efe4a22a8a95f
juye <kjy_1615@naver.com>
2024-07-02 18:41:01 +0900

    feat : 이미지 변경 중간커밋 - hotfix : 유저 테이블 컬럼 추가로인해 중간 커밋 발생

---
+++ src/main/java/app/bottlenote/user/dto/request/ProfileImageChangeRequest.java
@@ -0,0 +1,14 @@
+package app.bottlenote.user.dto.request;
+
+import jakarta.validation.constraints.NotNull;
+
+public record ProfileImageChangeRequest (
+
+	@NotNull
+	String viewUrl
+
+	){
+
+}
+
+
