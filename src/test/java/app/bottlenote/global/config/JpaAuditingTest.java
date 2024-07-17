6970a4ec9c6386f46d1da3f7b8b7fea4c6589b06
chadongmin <cdm2883@naver.com>
2024-07-18 00:03:28 +0900

    fix : import 오류 수정

--- src/test/java/app/bottlenote/global/config/JpaAuditingTest.java
+++ src/test/java/app/bottlenote/global/config/JpaAuditingTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.when;
 
+import app.bottlenote.global.jpa.AuditorAwareImpl;
 import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
