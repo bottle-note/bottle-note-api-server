580ab17308659a7db57e13e56db6400735d7906b
chadongmin <cdm2883@naver.com>
2024-07-17 21:40:10 +0900

    test : Auditing 기능 단위테스트 작성

---
+++ src/test/java/app/bottlenote/global/config/JpaAuditingTest.java
@@ -0,0 +1,72 @@
+package app.bottlenote.global.config;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@DisplayName("JPA Auditing 테스트")
+@ExtendWith(MockitoExtension.class)
+class JpaAuditingTest {
+
+	@Mock
+	private SecurityContext securityContext;
+
+	@Mock
+	private Authentication authentication;
+
+	@Mock
+	private UserDetails userDetails;
+
+	@InjectMocks
+	private AuditorAwareImpl auditorAware;
+
+	@BeforeEach
+	void setUp() {
+		SecurityContextHolder.setContext(securityContext);
+	}
+
+	@DisplayName("현재 로그인 한 유저가 Auditing 된다.")
+	@Test
+	void auditing_success_when_is_authenticated() {
+		// given
+		when(securityContext.getAuthentication()).thenReturn(authentication);
+		when(authentication.isAuthenticated()).thenReturn(true);
+		when(authentication.getPrincipal()).thenReturn(userDetails);
+		when(userDetails.getUsername()).thenReturn("testUser");
+
+		// then
+		Optional<String> currentAuditor = auditorAware.getCurrentAuditor();
+
+		// ghen
+		assertEquals("testUser", currentAuditor.get());
+	}
+
+	@DisplayName("로그인하지 않은 유저는 Auditing되지 않는다.")
+	@Test
+	void auditing_fail_when_is_not_authenticated() {
+		// Given
+		when(securityContext.getAuthentication()).thenReturn(authentication);
+		when(authentication.isAuthenticated()).thenReturn(false);
+
+		// When
+		Optional<String> currentAuditor = auditorAware.getCurrentAuditor();
+
+		// Then
+		assertTrue(currentAuditor.isEmpty());
+	}
+
+
+}
