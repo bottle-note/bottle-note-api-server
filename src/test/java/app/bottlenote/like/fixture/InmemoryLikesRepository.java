da5900b45cb50f82cf3260af3fe7f5ec64637b2c
whale0928 <rlagusrl928@gmail.com>
2024-07-18 22:49:05 +0900

    test: test-fixture class implementation

---
+++ src/test/java/app/bottlenote/like/fixture/InmemoryLikesRepository.java
@@ -0,0 +1,44 @@
+package app.bottlenote.like.fixture;
+
+import app.bottlenote.like.domain.Likes;
+import app.bottlenote.like.domain.LikesRepository;
+import app.bottlenote.review.fixture.InMemoryReviewRepository;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+public class InmemoryLikesRepository implements LikesRepository {
+	private static final Logger log = LogManager.getLogger(InMemoryReviewRepository.class);
+	Map<Long, Likes> database = new HashMap<>();
+
+	@Override
+	public Likes save(Likes likes) {
+		Long id = database.size() + 1L;
+		database.put(id, likes);
+		ReflectionTestUtils.setField(likes, "id", id);
+		log.info("[InMemory] likes repository save = {}", likes);
+		return likes;
+	}
+
+	@Override
+	public Optional<Likes> findById(Long id) {
+		return Optional.ofNullable(database.get(id));
+	}
+
+	@Override
+	public List<Likes> findAll() {
+		return List.copyOf(database.values());
+	}
+
+	@Override
+	public Optional<Likes> findByReviewIdAndUserId(Long reviewId, Long userId) {
+		return database.values().stream()
+			.filter(likes -> likes.getReview().getId().equals(reviewId) && likes.getUserInfo().getUserId().equals(userId))
+			.findFirst();
+	}
+}
