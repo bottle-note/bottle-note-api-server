19d3b356d66a7546d328c852659d1a0588c7345d
whale0928 <rlagusrl928@gmail.com>
2024-07-18 21:52:45 +0900

    feat: implementation of likes function for each layer with dto

---
+++ src/main/java/app/bottlenote/like/dto/response/LikesUpdateResponse.java
@@ -0,0 +1,37 @@
+package app.bottlenote.like.dto.response;
+
+import app.bottlenote.like.domain.LikeStatus;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+public record LikesUpdateResponse(
+	String message,
+	Long reviewId,
+	Long userId,
+	String userNickName,
+	LikeStatus status
+) {
+	public static LikesUpdateResponse of(
+		Long reviewId,
+		Long userId,
+		String userNickName,
+		LikeStatus status
+	) {
+		return new LikesUpdateResponse(
+			status == LikeStatus.LIKE ? Message.LIKED.message : Message.DISLIKE.message,
+			reviewId,
+			userId,
+			userNickName,
+			status
+		);
+	}
+
+	@Getter
+	@AllArgsConstructor
+	public enum Message {
+		LIKED("정상적으로 좋아요 처리 되었습니다."),
+		DISLIKE("정상적으로 좋아요 취소 처리 되었습니다.");
+
+		private final String message;
+	}
+}
