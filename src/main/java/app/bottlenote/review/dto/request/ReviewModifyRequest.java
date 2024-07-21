3ed9dfd524de3bcc8e35bbbe1d7e3729829abf85
chadongmin <cdm2883@naver.com>
2024-07-21 17:00:13 +0900

    fix : null 값 허용, 필드 미포함은 허용x

--- src/main/java/app/bottlenote/review/dto/request/ReviewModifyRequest.java
+++ src/main/java/app/bottlenote/review/dto/request/ReviewModifyRequest.java
@@ -2,18 +2,19 @@ package app.bottlenote.review.dto.request;
 
 import app.bottlenote.review.domain.constant.ReviewDisplayStatus;
 import app.bottlenote.review.domain.constant.SizeType;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.DecimalMax;
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-
 import java.math.BigDecimal;
 import java.util.List;
 
-
 public record ReviewModifyRequest(
+
 	@NotEmpty(message = "리뷰 내용을 입력해주세요")
 	@Size(max = 500)
 	String content,
@@ -23,20 +24,25 @@ public record ReviewModifyRequest(
 
 	@DecimalMin(value = "0.0", message = "가격은 0 이상이어야 합니다.")
 	@DecimalMax(value = "1000000000000", message = "입력할 수 있는 가격의 범위가 아닙니다.")
-	@NotNull
+	@JsonInclude()
+	@JsonProperty(required = true)
 	BigDecimal price,
 
-	@NotNull
+	@JsonInclude()
+	@JsonProperty(required = true)
 	List<ReviewImageInfo> imageUrlList,
 
-	@NotNull
+	@JsonInclude()
+	@JsonProperty(required = true)
 	SizeType sizeType,
 
-	@NotNull
+	@JsonInclude()
+	@JsonProperty(required = true)
 	List<String> tastingTagList,
 
 	@Valid
-	@NotNull
+	@JsonInclude()
+	@JsonProperty(required = true)
 	LocationInfo locationInfo
 
 ) {
