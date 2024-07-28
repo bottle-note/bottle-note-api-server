9ba1dadb0d796c2845f5c8059af470eb2e839906
chadongmin <cdm2883@naver.com>
2024-07-28 13:46:47 +0900

    feat : 회원탈퇴 컨트롤러 구현

--- src/main/java/app/bottlenote/user/controller/UserCommandController.java
+++ src/main/java/app/bottlenote/user/controller/UserCommandController.java
@@ -1,9 +1,12 @@
 package app.bottlenote.user.controller;
 
+import static app.bottlenote.global.security.SecurityContextUtil.getUserIdByContext;
+
 import app.bottlenote.global.data.response.GlobalResponse;
 import app.bottlenote.user.dto.request.NicknameChangeRequest;
 import app.bottlenote.user.dto.response.NicknameChangeResponse;
 import app.bottlenote.user.dto.response.ProfileImageChangeResponse;
+import app.bottlenote.user.dto.response.UserWithdrawResponse;
 import app.bottlenote.user.exception.UserException;
 import app.bottlenote.user.exception.UserExceptionCode;
 import app.bottlenote.user.service.UserCommandService;
@@ -11,12 +14,11 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import static app.bottlenote.global.security.SecurityContextUtil.getUserIdByContext;
 
 
 @Slf4j
@@ -47,6 +49,17 @@ public class UserCommandController {
 			.orElseThrow(() -> new UserException(UserExceptionCode.REQUIRED_USER_ID));
 
 		ProfileImageChangeResponse response = userCommandService.profileImageChange(userId, viewUrl);
+
+		return ResponseEntity.ok(GlobalResponse.success(response));
+	}
+
+	@DeleteMapping()
+	public ResponseEntity<GlobalResponse> withdrawUser() {
+
+		Long userId = getUserIdByContext()
+			.orElseThrow(() -> new UserException(UserExceptionCode.REQUIRED_USER_ID));
+
+		UserWithdrawResponse response = userCommandService.withdrawUser(userId);
 
 		return ResponseEntity.ok(GlobalResponse.success(response));
 	}
