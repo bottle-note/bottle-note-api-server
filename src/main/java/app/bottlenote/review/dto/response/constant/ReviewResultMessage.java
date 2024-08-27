4d39cedb69087b8f6551e216980229896f89582f
chadongmin <cdm2883@naver.com>
2024-08-28 00:23:45 +0900

    feat : 상태 변경에 따른 ResultMessage 추가

--- src/main/java/app/bottlenote/review/dto/response/constant/ReviewResultMessage.java
+++ src/main/java/app/bottlenote/review/dto/response/constant/ReviewResultMessage.java
@@ -8,7 +8,9 @@ public enum ReviewResultMessage {
 	MODIFY_SUCCESS("리뷰 수정이 성공적으로 완료되었습니다."),
 	DELETE_SUCCESS("리뷰 삭제가 성공적으로 완료되었습니다."),
 	BLOCK_SUCCESS("해당 리뷰가 비활성화 되었습니다."),
-	ACTIVE_SUCCESS("해당 리뷰 활성화 되었습니다.");
+	ACTIVE_SUCCESS("해당 리뷰가 활성화 되었습니다."),
+	PUBLIC_SUCCESS("해당 리뷰가 공개 처리되었습니다."),
+	PRIVATE_SUCCESS("해당 리뷰가 비공개 처리되었습니다.");
 
 	private final String description;
 
