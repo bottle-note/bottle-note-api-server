2b7c5ecabed4880a675d385ed6d26e15cf7c07da
chadongmin <cdm2883@naver.com>
2024-09-12 14:38:39 +0900

    refactor : 단위 테스트 작성

--- src/test/java/app/bottlenote/support/help/service/HelpServiceTest.java
+++ src/test/java/app/bottlenote/support/help/service/HelpServiceTest.java
@@ -1,6 +1,7 @@
 package app.bottlenote.support.help.service;
 
 import app.bottlenote.support.help.domain.Help;
+import app.bottlenote.support.help.domain.constant.HelpType;
 import app.bottlenote.support.help.dto.request.HelpUpsertRequest;
 import app.bottlenote.support.help.dto.response.HelpUpsertResponse;
 import app.bottlenote.support.help.fixture.HelpObjectFixture;
@@ -15,11 +16,17 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
+
+import static app.bottlenote.support.help.dto.response.constant.HelpResultMessage.MODIFY_SUCCESS;
 import static app.bottlenote.support.help.dto.response.constant.HelpResultMessage.REGISTER_SUCCESS;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.when;
 
 @Tag("unit")
 @DisplayName("[unit] [service] HelpService")
@@ -35,18 +42,12 @@ class HelpServiceTest {
 	@Mock
 	private UserDomainSupport userDomainSupport;
 
+	private final HelpUpsertRequest helpUpsertRequest = HelpObjectFixture.getHelpUpsertRequest();
+	private final Help help = HelpObjectFixture.getHelpDefaultFixture();
+
 	@DisplayName("회원은 문의글을 작성할 수 있다.")
 	@Test
 	void testUserRegisterHelp_Success() {
-		// given
-
-		HelpUpsertRequest helpUpsertRequest = HelpObjectFixture.getHelpRegisterRequest();
-
-		Help help = Help.create(1L,
-			helpUpsertRequest.title(),
-			helpUpsertRequest.content(),
-			helpUpsertRequest.type()
-		);
 
 		//when
 		doNothing().when(userDomainSupport).isValidUserId(anyLong());
@@ -61,9 +62,6 @@ class HelpServiceTest {
 	@Test
 	void testHelpRegister_fail_when_unauthorized_user() {
 
-		// given
-		HelpUpsertRequest helpUpsertRequest = HelpObjectFixture.getHelpRegisterRequest();
-
 		// when
 		doThrow(UserException.class).when(userDomainSupport).isValidUserId(anyLong());
 
@@ -71,5 +69,41 @@ class HelpServiceTest {
 		assertThrows(UserException.class, () -> helpService.registerHelp(helpUpsertRequest, 1L));
 	}
 
+	@DisplayName("문의글을 수정할 수 있다.")
+	@Test
+	void testHelpUpdate_success() {
+	    // given
+		HelpUpsertRequest updateRequest = new HelpUpsertRequest("수정 후 제목","수정 후 내용", HelpType.USER);
 
+	    // when
+		when(helpRepository.findByIdAndUserId(anyLong(), anyLong()))
+			.thenReturn(Optional.of(help));
+
+		HelpUpsertResponse response = helpService.modifyHelp(updateRequest, 1L, 1L);
+
+		// then
+		assertEquals(MODIFY_SUCCESS, response.codeMessage());
+	}
+
+	@DisplayName("유저는 본인이 작성한 문의글만 수정할 수 있다.")
+	@Test
+	void testHelpUpdate_fail_when_user_is_not_owner() {
+	    // given
+
+	    // when
+		when(helpRepository.findByIdAndUserId(anyLong(), anyLong()))
+			.thenThrow(UserException.class);
+
+	    // then
+		assertThrows(UserException.class,
+			() -> helpService.modifyHelp(helpUpsertRequest, 1L, 1L));
+	}
+
+	@DisplayName("수정 요청에 null인 필드는 허용하지 않는다.")
+	@Test
+	void testHelpUpdate_fail_when_request_contains_null() {
+
+		assertThrows(NullPointerException.class,
+			() -> help.updateHelp("title",null,HelpType.USER ));
+	}
 }
