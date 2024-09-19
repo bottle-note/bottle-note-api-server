ac09d66dd0f55d4ceaab296e6cec63334cdb24b1
chadongmin <cdm2883@naver.com>
2024-09-19 21:28:18 +0900

    feat : 문의글 목록 조회를 위한 Pageable 객체와 응답 리스트 객체 생성

---
+++ src/main/java/app/bottlenote/support/help/dto/response/HelpListResponse.java
@@ -0,0 +1,29 @@
+package app.bottlenote.support.help.dto.response;
+
+import lombok.Builder;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public record HelpListResponse(
+	Long totalCount,
+	List<HelpInfo> helpList
+
+){
+	public static HelpListResponse of(Long totalCount, List<HelpInfo> helpList){
+		return new HelpListResponse(totalCount, helpList);
+	}
+
+	@Builder
+	public record HelpInfo(
+		Long helpId,
+		String title,
+		LocalDateTime createAt
+	){
+
+		public static HelpInfo of(Long helpId, String title, LocalDateTime createAt){
+			return new HelpInfo(helpId, title, createAt);
+		}
+	}
+}
+
