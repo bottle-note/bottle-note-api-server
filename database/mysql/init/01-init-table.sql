fe356de1e85cd31ed0bef3386b0ca1147e66ccb6
chadongmin <cdm2883@naver.com>
2024-09-20 19:42:54 +0900

    refactor : alcohol_id 외래키 추가

--- database/mysql/init/01-init-table.sql
+++ database/mysql/init/01-init-table.sql
@@ -359,11 +359,11 @@ create table user_history
 (
     id              bigint       not null AUTO_INCREMENT comment '히스토리 id',
     user_id         bigint       not null comment '사용자 id',
+    alcohol_id      bigint       not null comment '알코올 id',
     event_category  varchar(255) not null comment 'pick, review, rating',
-    event_type      varchar(255) null comment 'isPick,unPick || like, create, review, best || start, modify, delete',
+    event_type      varchar(255) null comment 'isPick, unPick || like, create, review, best || start, modify, delete',
     redirect_url    varchar(255) null comment '발생되는 api의 도메인주소를 뺀 url',
     image_url       varchar(255) null comment '발생되는 api의 도메인주소를 뺀 url',
-    alcohol_id      bigint       null comment '알코올 이름(한글)',
     message         varchar(255) null comment '이벤트 메세지 enum으로 관리',
     dynamic_message json         null comment '가변데이터(현재는 별점에서만 사용)',
     event_year      varchar(255) null comment '발생 년(YYYY)',
@@ -375,7 +375,9 @@ create table user_history
     last_modify_by  varchar(255) null comment '최종 생성자',
     PRIMARY KEY (`id`),
     constraint user_history_ibfk_1
-        foreign key (user_id) references users (id)
+        foreign key (user_id) references users (id),
+    constraint user_history_ibfk_2
+        foreign key (alcohol_id) references alcohol (id)
 )
     engine = InnoDB
     default charset = utf8mb4
