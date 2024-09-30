a947fe7e51500f6d0eb942e6f21791a5961852b1
chadongmin <cdm2883@naver.com>
2024-09-30 21:25:18 +0900

    docs : rest docs 문서 작성

--- src/test/java/app/bottlenote/docs/support/help/RestDocsHelpCommandControllerTest.java
+++ src/test/java/app/bottlenote/docs/support/help/RestDocsHelpCommandControllerTest.java
@@ -2,8 +2,13 @@ package app.bottlenote.docs.support.help;
 
 import app.bottlenote.docs.AbstractRestDocs;
 import app.bottlenote.global.security.SecurityContextUtil;
+import app.bottlenote.global.service.cursor.PageResponse;
 import app.bottlenote.support.help.controller.HelpCommandController;
+import app.bottlenote.support.help.domain.constant.HelpType;
+import app.bottlenote.support.help.dto.request.HelpPageableRequest;
 import app.bottlenote.support.help.dto.request.HelpUpsertRequest;
+import app.bottlenote.support.help.dto.response.HelpDetailInfo;
+import app.bottlenote.support.help.dto.response.HelpListResponse;
 import app.bottlenote.support.help.dto.response.HelpResultResponse;
 import app.bottlenote.support.help.fixture.HelpObjectFixture;
 import app.bottlenote.support.help.service.HelpService;
@@ -28,8 +33,11 @@ import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.docu
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.queryParameters;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -41,6 +49,8 @@ class RestDocsHelpCommandControllerTest extends AbstractRestDocs {
 
 	private final MockedStatic<SecurityContextUtil> mockedSecurityUtil = mockStatic(SecurityContextUtil.class);
 
+	private final PageResponse<HelpListResponse> helpPageResponse = HelpObjectFixture.getHelpListPageResponse();
+	private final HelpDetailInfo helpDetailInfo = HelpObjectFixture.getDetailHelpInfo("title", "content", HelpType.USER);
 	private final HelpUpsertRequest helpUpsertRequest = HelpObjectFixture.getHelpUpsertRequest();
 	private final HelpResultResponse successRegisterResponse = HelpObjectFixture.getSuccessHelpResponse(REGISTER_SUCCESS);
 	private final HelpResultResponse successModifyResponse = HelpObjectFixture.getSuccessHelpResponse(MODIFY_SUCCESS);
@@ -97,6 +107,93 @@ class RestDocsHelpCommandControllerTest extends AbstractRestDocs {
 					)
 				)
 			);
+	}
+
+	@Test
+	@DisplayName("문의글을 목록을 조회할 수 있다.")
+	void help_read_list_test() throws Exception {
+
+		Long userId = 1L;
+
+		//when
+		when(SecurityContextUtil.getUserIdByContext()).thenReturn(Optional.of(userId));
+
+		when(helpService.getHelpList(any(HelpPageableRequest.class), anyLong()))
+			.thenReturn(helpPageResponse);
+
+		//then
+		mockMvc.perform(get("/api/v1/help")
+				.contentType(MediaType.APPLICATION_JSON)
+				.param("cursor", "0")
+				.param("pageSize", "2")
+			)
+			.andExpect(status().isOk())
+			.andDo(
+				document("support/help/help-read-list",
+					queryParameters(
+						parameterWithName("cursor").optional().description("조회 할 시작 기준 위치"),
+						parameterWithName("pageSize").optional().description("조회 할 페이지 사이즈")
+					),
+					responseFields(
+						fieldWithPath("success").description("응답 성공 여부"),
+						fieldWithPath("code").description("응답 코드(http status code)"),
+						fieldWithPath("data.totalCount").description("성공 메시지 코드"),
+						fieldWithPath("data.helpList[].helpId").description("문의글 ID"),
+						fieldWithPath("data.helpList[].title").description("문의글 제목"),
+						fieldWithPath("data.helpList[].createAt").description("문의글 등록일시"),
+						fieldWithPath("errors").ignored(),
+						fieldWithPath("meta.serverEncoding").ignored(),
+						fieldWithPath("meta.serverVersion").ignored(),
+						fieldWithPath("meta.serverPathVersion").ignored(),
+						fieldWithPath("meta.serverResponseTime").ignored(),
+						fieldWithPath("meta.pageable").description("페이징 정보"),
+						fieldWithPath("meta.pageable.currentCursor").description("조회 시 기준 커서"),
+						fieldWithPath("meta.pageable.cursor").description("다음 페이지 커서"),
+						fieldWithPath("meta.pageable.pageSize").description("조회된 페이지 사이즈"),
+						fieldWithPath("meta.pageable.hasNext").description("다음 페이지 존재 여부")
+					)
+				)
+			);
+	}
+
+		@Test
+		@DisplayName("문의글을 상세 조회할 수 있다.")
+		void help_read_detail_test() throws Exception {
+
+			Long userId = 1L;
+
+			//when
+			when(SecurityContextUtil.getUserIdByContext()).thenReturn(Optional.of(userId));
+
+			when(helpService.getDetailHelp(anyLong(), anyLong()))
+				.thenReturn(helpDetailInfo);
+
+			//then
+			mockMvc.perform(get("/api/v1/help/{helpId}", 1L)
+					.contentType(MediaType.APPLICATION_JSON)
+				)
+				.andExpect(status().isOk())
+				.andDo(
+					document("support/help/help-read-detail",
+						responseFields(
+							fieldWithPath("success").description("응답 성공 여부"),
+							fieldWithPath("code").description("응답 코드(http status code)"),
+							fieldWithPath("data.helpId").description("문의글 ID"),
+							fieldWithPath("data.title").description("문의글 제목"),
+							fieldWithPath("data.content").description("문의글 내용"),
+							fieldWithPath("data.helpType").description("문의글 타입"),
+							fieldWithPath("data.createAt").description("문의글 등록일시"),
+							fieldWithPath("data.adminId").description("시스템 관리자 ID"),
+							fieldWithPath("data.responseContent").description("시스템 관리자 답변"),
+							fieldWithPath("data.lastModifyAt").description("최종 수정 일자"),
+							fieldWithPath("errors").ignored(),
+							fieldWithPath("meta.serverEncoding").ignored(),
+							fieldWithPath("meta.serverVersion").ignored(),
+							fieldWithPath("meta.serverPathVersion").ignored(),
+							fieldWithPath("meta.serverResponseTime").ignored()
+						)
+					)
+				);
 	}
 
 	@Test
