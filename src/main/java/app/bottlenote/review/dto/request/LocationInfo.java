53b4f6918b0aa01867a3ca585ab780db6dea123b
chadongmin <cdm2883@naver.com>
2024-10-13 14:35:42 +0900

    fix : 도로명주소 not null 조건 삭제

--- src/main/java/app/bottlenote/review/dto/request/LocationInfo.java
+++ src/main/java/app/bottlenote/review/dto/request/LocationInfo.java
@@ -1,15 +1,11 @@
 package app.bottlenote.review.dto.request;
 
-import jakarta.validation.constraints.NotNull;
-
 public record LocationInfo(
 
 	String locationName,
 
-	@NotNull(message = "STREET_ADDRESS_REQUIRED")
 	String streetAddress,
 
-	//TODO : Enum으로 관리 필요
 	String category,
 
 	String mapUrl,
