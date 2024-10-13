3fb8b5f51b316ca0396f5187b55f2cc8f282309c
chadongmin <cdm2883@naver.com>
2024-10-13 14:14:00 +0900

    refactor : ApplicationEventPublisher 인터페이스를 주입받도록 수정

--- src/main/java/app/bottlenote/review/event/publisher/ReviewEventPublisher.java
+++ src/main/java/app/bottlenote/review/event/publisher/ReviewEventPublisher.java
@@ -1,37 +1,40 @@
 package app.bottlenote.review.event.publisher;
 
+import app.bottlenote.history.domain.constant.EventCategory;
+import app.bottlenote.history.domain.constant.EventType;
 import app.bottlenote.history.dto.payload.HistoryEvent;
 import app.bottlenote.review.dto.payload.ReviewRegistryEvent;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.ApplicationEvent;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
-public class ReviewEventPublisher implements ApplicationEventPublisher {
+public class ReviewEventPublisher {
+	private final ApplicationEventPublisher eventPublisher;
 
-	@Override
-	public void publishEvent(ApplicationEvent event) {
-		ApplicationEventPublisher.super.publishEvent(event);
-	}
-
-	@Override
-	public void publishEvent(final Object event) {
+	public ReviewEventPublisher(ApplicationEventPublisher applicationEventPublisher) {
+		this.eventPublisher = applicationEventPublisher;
 	}
 
 	public void reviewRegistry(ReviewRegistryEvent registryEvent) {
 		log.info("ReviewRegistryEvent: {}", registryEvent);
 
-		//ReviewRegistryEvent 를  HistoryEvent 로 변경 해서 이벤트 발행
-
-		publishEvent(registryEvent);
+		HistoryEvent reviewCreateHistoryEvent = HistoryEvent.makeHistoryEvent(
+			registryEvent.userId(),
+			EventCategory.REVIEW,
+			EventType.REVIEW_CREATE,
+			"redirectUrl",
+			registryEvent.alcoholId(),
+			"리뷰 등록",
+			null,
+			"리뷰가 등록되었습니다."
+		);
+		eventPublisher.publishEvent(reviewCreateHistoryEvent);
 	}
 
 	public void reviewUpdate(Object o) {
 		log.info("ReviewUpdateEvent: {}", o);
-		publishEvent(o);
+		eventPublisher.publishEvent(o);
 	}
-
-
 }
