d3d1ca39da7fa3fc4c8e1a9c372ce12cb89b882f
chadongmin <cdm2883@naver.com>
2024-10-14 10:24:45 +0900

    refactor : 엔티티 변경 (문의글 이미지 추가)

--- src/main/java/app/bottlenote/support/help/domain/Help.java
+++ src/main/java/app/bottlenote/support/help/domain/Help.java
@@ -3,19 +3,24 @@ package app.bottlenote.support.help.domain;
 import app.bottlenote.common.domain.BaseEntity;
 import app.bottlenote.support.constant.StatusType;
 import app.bottlenote.support.help.domain.constant.HelpType;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.Comment;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Comment("문의사항")
@@ -37,13 +42,13 @@ public class Help extends BaseEntity {
 	@Enumerated(EnumType.STRING)
 	private HelpType type;
 
-	@Comment("문의 제목")
-	@Column(name = "title", nullable = false)
-	private String title;
-
 	@Comment("문의 내용")
 	@Column(name = "help_content", nullable = false)
 	private String content;
+
+	@Comment("문의 이미지")
+	@OneToMany(mappedBy = "help", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<HelpImage> helpImages = new ArrayList<>();
 
 	@Comment("문의글의 처리 상태")
 	@Enumerated(EnumType.STRING)
@@ -59,30 +64,36 @@ public class Help extends BaseEntity {
 	private String responseContent;
 
 	@Builder
-	private Help(Long id, Long userId, HelpType type, String title, String content, Long adminId, String responseContent) {
+	private Help(Long id, Long userId, HelpType type, String content, Long adminId, String responseContent) {
 		this.id = id;
 		this.userId = userId;
 		this.type = type;
-		this.title = title;
 		this.content = content;
 		this.adminId = adminId;
 		this.responseContent = responseContent;
+		this.helpImages = new ArrayList<>();
 	}
 
-	public static Help create(Long userId, String title, String content, HelpType helpType) {
+	public static Help create(Long userId, HelpType helpType, String content) {
 		return Help.builder()
 			.userId(userId)
-			.title(title)
-			.content(content)
 			.type(helpType)
+			.content(content)
 			.build();
 	}
 
-	public void updateHelp(String title, String content, HelpType helpType){
-		Objects.requireNonNull(title, "title은 필수입니다");
+	public void saveImage(List<HelpImage> helpImage) {
+		this.helpImages.addAll(helpImage);
+	}
+
+	public void updateImages(List<HelpImage> helpImages) {
+		this.helpImages.clear();
+		this.helpImages.addAll(helpImages);
+	}
+
+	public void updateHelp(String content, HelpType helpType){
 		Objects.requireNonNull(content, "content는 필수입니다");
 		Objects.requireNonNull(helpType, "helpType은 필수입니다");
-		this.title = title;
 		this.content = content;
 		this.type = helpType;
 	}
