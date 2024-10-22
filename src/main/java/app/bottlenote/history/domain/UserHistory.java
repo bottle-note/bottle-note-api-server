80a1f467d57773827782cb3c5733fda7846754ff
chadongmin <cdm2883@naver.com>
2024-10-22 23:51:43 +0900

    refactor : HistoryEvent 페이로드 객체 및 UserHistory 엔티티 유효성 검증 로직 추가

--- src/main/java/app/bottlenote/history/domain/UserHistory.java
+++ src/main/java/app/bottlenote/history/domain/UserHistory.java
@@ -1,6 +1,8 @@
 package app.bottlenote.history.domain;
 
 import app.bottlenote.common.domain.BaseEntity;
+import app.bottlenote.common.exception.CommonException;
+import app.bottlenote.common.exception.CommonExceptionCode;
 import app.bottlenote.history.domain.constant.EventCategory;
 import app.bottlenote.history.domain.constant.EventType;
 import io.hypersistence.utils.hibernate.type.json.JsonType;
@@ -13,6 +15,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -72,12 +75,20 @@ public class UserHistory extends BaseEntity {
 		this.eventCategory = eventCategory;
 		this.eventType = eventType;
 		this.redirectUrl = redirectUrl;
-		this.imageUrl = imageUrl;
+		this.imageUrl = validateImageUrl(imageUrl);
 		this.alcoholId = alcoholId;
 		this.message = message;
 		this.dynamicMessage = dynamicMessage;
 		this.eventYear = eventYear;
 		this.eventMonth = eventMonth;
 		this.description = description;
+	}
+
+	private String validateImageUrl(String imageUrl) {
+		Objects.requireNonNull(imageUrl, "imageUrl must not be null");
+		if (imageUrl.startsWith("http://") || imageUrl.startsWith("https://")) {
+			return imageUrl;
+		}
+		throw new CommonException(CommonExceptionCode.INVALID_IMAGE_URL);
 	}
 }
