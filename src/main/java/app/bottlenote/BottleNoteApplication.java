5b9b18890718a200a19d352dff3a6d71ff79a240
whale0928 <rlagusrl928@gmail.com>
2024-10-27 21:38:03 +0900

    feat: update fcm config

--- src/main/java/app/bottlenote/BottleNoteApplication.java
+++ src/main/java/app/bottlenote/BottleNoteApplication.java
@@ -1,13 +1,17 @@
 package app.bottlenote;
 
+import app.external.FirebaseInitializer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "app")
 public class BottleNoteApplication {
 	public static void main(String[] args) {
+
+		FirebaseInitializer.initialize();
+
 		SpringApplication.run(BottleNoteApplication.class, args);
 	}
 }
