332eca1e45481ccd7a0e3557aa7413027f645ddd
whale0928 <rlagusrl928@gmail.com>
2024-10-27 21:49:02 +0900

    feat: update path

--- src/main/java/app/bottlenote/BottleNoteApplication.java
+++ src/main/java/app/bottlenote/BottleNoteApplication.java
@@ -1,6 +1,5 @@
 package app.bottlenote;
 
-import app.external.FirebaseInitializer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -9,9 +8,6 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication(scanBasePackages = "app")
 public class BottleNoteApplication {
 	public static void main(String[] args) {
-
-		FirebaseInitializer.initialize();
-
 		SpringApplication.run(BottleNoteApplication.class, args);
 	}
 }
