06f0c49ddd51c25b5982935d923db573d34ff638
hgkim <hgkim@openrd.com>
2024-11-03 20:24:44 +0900

    feat: add base scan package

--- src/main/java/app/bottlenote/BottleNoteApplication.java
+++ src/main/java/app/bottlenote/BottleNoteApplication.java
@@ -2,9 +2,11 @@ package app.bottlenote;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
+@EntityScan(basePackages = "app")
 @SpringBootApplication(scanBasePackages = "app")
 public class BottleNoteApplication {
 	public static void main(String[] args) {
--- src/main/java/app/bottlenote/global/config/jpa/JpaConfig.java
+++ src/main/java/app/bottlenote/global/config/jpa/JpaConfig.java
@@ -4,11 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
  * JPA 전역 설정 클래스
  */
 @EnableJpaAuditing
+@EnableJpaRepositories(basePackages = "app")
 @Configuration
 public class JpaConfig {
 
@@ -18,4 +20,3 @@ public class JpaConfig {
 	}
 
 }
-
