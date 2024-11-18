1a63cc3eb00d0a396301479761a53a29542181f3
hgkim <hgkim@openrd.com>
2024-11-18 22:08:14 +0900

    fix: pointless test disabled

--- src/test/java/app/bottlenote/user/service/JwtTokenValidatorTest.java
+++ src/test/java/app/bottlenote/user/service/JwtTokenValidatorTest.java
@@ -5,6 +5,7 @@ import app.bottlenote.global.security.jwt.JwtTokenValidator;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
@@ -23,17 +24,16 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 class JwtTokenValidatorTest {
 
 	@Test
+	@Disabled
 	@DisplayName("리프레시 토큰이 만료되었을떄, 토큰 검증 메서드는 IllegalArgumentException을 던진다.")
 	void test_token_validator_when_refresh_token_is_expired() {
-
 		Date now = new Date();
 
 		String tmpRefreshToken = Jwts.builder()
 			.setClaims(Jwts.claims().setSubject("cha"))
 			.setIssuedAt(now)
 			.setExpiration(new Date(now.getTime() - 3600 * 1000))
-			.signWith(Keys.hmacShaKeyFor("c2VjdXJlU2VjcmV0S2V5MTIzNDU2Nzg5MGFiY2RlZmdoaWprbG1ub3BxcnN0dXZ3eHl6QWJDRGVGR2hJSktMTU5PUFFSU1RVVldYWVphYmNkZWZnaGlrSg==".getBytes()),
-				SignatureAlgorithm.HS512)
+			.signWith(Keys.hmacShaKeyFor("c2VjdXJlU2VjcmV0S2V5MTIzNDU2Nzg5MGFiY2RlZmdoaWprbG1ub3BxcnN0dXZ3eHl6QWJDRGVGR2hJSktMTU5PUFFSU1RVVldYWVphYmNkZWZnaGlrSg==".getBytes()), SignatureAlgorithm.HS512)
 			.compact();
 
 		assertThrows(IllegalArgumentException.class, () -> JwtTokenValidator.validateToken(tmpRefreshToken));
