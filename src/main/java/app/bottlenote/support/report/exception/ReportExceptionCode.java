5292c2011524a39e508eb67cb1e46d8b43166bc3
chadongmin <cdm2883@naver.com>
2024-12-05 21:03:56 +0900

    refactor : 검증로직 및 리뷰 자동 Disable처리 로직 추가
    - 사용자는 하나의 리뷰는 한번만 신고 가능
    - 같은 IP는 하나의 신고로 간주함
    - 리뷰 5회 이상 신고 누적시 자동 DISABLE 처리

--- src/main/java/app/bottlenote/support/report/exception/ReportExceptionCode.java
+++ src/main/java/app/bottlenote/support/report/exception/ReportExceptionCode.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 public enum ReportExceptionCode implements ExceptionCode {
 	REPORT_USER_NOT_FOUND(HttpStatus.NOT_FOUND, "신고 대상 유저를 찾을 수 없습니다."),
 	ALREADY_REPORTED_USER(HttpStatus.BAD_REQUEST, "이미 신고한 사용자입니다."),
+	ALREADY_REPORTED_REVIEW(HttpStatus.BAD_REQUEST, "이미 신고한 리뷰입니다."),
 	REPORT_CONTENT_NOT_VALID(HttpStatus.BAD_REQUEST, "신고 내용이 유효하지 않습니다."),
 	REPORT_CONTENT_OVERFLOW(HttpStatus.BAD_REQUEST, "신고 내용이 300자를 초과했습니다."),
 	REPORT_TYPE_NOT_VALID(HttpStatus.BAD_REQUEST, "신고 타입이 유효하지 않습니다."),
