ca455d335433e0537e6a94f6cca346c86ccd13e6
hgkim <hgkim@openrd.com>
2024-12-08 16:12:41 +0900

    docs: update http document

--- src/main/java/app/external/push/dto/request/TokenSaveRequest.java
+++ src/main/java/app/external/push/dto/request/TokenSaveRequest.java
@@ -1,12 +1,13 @@
 package app.external.push.dto.request;
 
 import app.external.push.domain.Platform;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public record TokenSaveRequest(
-        @NotNull(message = "DEVICE_TOKEN_REQUIRED")
-        String deviceToken,
-        @NotNull(message = "PLATFORM_REQUIRED")
-        Platform platform
+	@NotBlank(message = "DEVICE_TOKEN_REQUIRED")
+	String deviceToken,
+	@NotNull(message = "PLATFORM_REQUIRED")
+	Platform platform
 ) {
 }
