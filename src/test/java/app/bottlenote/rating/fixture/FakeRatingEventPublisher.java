1734e00df84d5451284f4aa76c8f112d1a964d2a
hgkim <hgkim@openrd.com>
2024-12-19 23:07:08 +0900

    feat: add todo comment

--- src/test/java/app/bottlenote/rating/fixture/FakeRatingEventPublisher.java
+++ src/test/java/app/bottlenote/rating/fixture/FakeRatingEventPublisher.java
@@ -3,9 +3,10 @@ package app.bottlenote.rating.fixture;
 import app.bottlenote.history.event.publisher.HistoryEventPublisher;
 
 public class FakeRatingEventPublisher implements HistoryEventPublisher {
-	
+
 	@Override
 	public void publishHistoryEvent(Object event) {
+		//todo : 구현 필요
 		// dummy object
 	}
 }
