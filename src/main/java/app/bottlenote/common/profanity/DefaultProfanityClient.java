e7d192cd58d5e494d2ade8fca4d4a9b2608a63f3
Living whale <rlagusrl928@gmail.com>
2025-02-07 00:52:08 +0900

    chore: profanity filter api linkage (#318)
    Co-authored-by: hgkim <hgkim@openrd.com>

--- src/main/java/app/bottlenote/common/profanity/DefaultProfanityClient.java
+++ src/main/java/app/bottlenote/common/profanity/DefaultProfanityClient.java
@@ -6,40 +6,32 @@ import app.bottlenote.common.exception.CommonExceptionCode;
 import app.bottlenote.common.profanity.fegin.ProfanityFeignClient;
 import app.bottlenote.common.profanity.request.ProfanityRequest;
 import app.bottlenote.common.profanity.response.ProfanityResponse;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+
+import java.util.Objects;
 
 /**
  * 비속어 검출 클라이언트의 기본 구현체
  */
+@Slf4j
 @Component
+@RequiredArgsConstructor
 public class DefaultProfanityClient implements ProfanityClient {
-
-	private static final Logger log = LogManager.getLogger(DefaultProfanityClient.class);
 	private final ProfanityFeignClient profanityFeignClient;
-
-	public DefaultProfanityClient(ProfanityFeignClient profanityFeignClient) {
-		this.profanityFeignClient = profanityFeignClient;
-	}
 
 	@Override
 	public ProfanityResponse requestVerificationProfanity(String text) {
 		log.info("[requestVerificationProfanity] 검증 요청 대상: {}", text);
-
 		long start = System.currentTimeMillis();
-
 		ProfanityRequest request = ProfanityRequest.createFilter(text);
 		ResponseEntity<ProfanityResponse> response = profanityFeignClient.requestVerificationProfanity(request);
 		var responseBody = response.getBody();
-
 		long end = System.currentTimeMillis();
 
-		log.info("응답 시간 : {} ms", end - start);  // 응답 시간 로그 출력
-
-		log.info("검증 완료 : [ Code: {}] ,[Header: {}]  ", response.getStatusCode(), response.getHeaders());
-
+		log.info("검증 완료 : [ Code: {}] ,[Header: {}] 응답 시간 : {} ms\"  ", Objects.requireNonNull(responseBody).status().code(), responseBody.status().message(), end - start);
 		return responseBody;
 	}
 
