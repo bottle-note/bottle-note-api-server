67531ea265d9d49023fe4da088561c9947d3f036
chadongmin <cdm2883@naver.com>
2025-02-24 11:40:05 +0900

    refactor : NPE 방어로직 추가

--- src/main/java/app/bottlenote/review/dto/response/ReviewDetailResponse.java
+++ src/main/java/app/bottlenote/review/dto/response/ReviewDetailResponse.java
@@ -11,10 +11,15 @@ public record ReviewDetailResponse(
 	ReviewInfo reviewInfo,
 	List<ReviewImageInfo> reviewImageList
 ) {
+	public ReviewDetailResponse {
+		reviewInfo = (reviewInfo != null) ? reviewInfo : ReviewInfo.builder().build();
+		reviewImageList = (reviewImageList != null) ? reviewImageList : List.of();
+	}
+
 	public static ReviewDetailResponse create(AlcoholInfo alcoholInfo, ReviewInfo reviewInfo, List<ReviewImageInfo> reviewImageList) {
 		if (reviewInfo == null) {
-			return new ReviewDetailResponse(alcoholInfo, null, null);
+			return new ReviewDetailResponse(alcoholInfo, ReviewInfo.builder().build(), List.of());
 		}
-		return new ReviewDetailResponse(alcoholInfo, reviewInfo, reviewImageList);
+		return new ReviewDetailResponse(alcoholInfo, reviewInfo, reviewImageList != null ? reviewImageList : List.of());
 	}
 }
