3285367649bfcbaab4d80adbd836023f0fc59d67
chadongmin <cdm2883@naver.com>
2025-04-02 00:12:05 +0900

    feat : 애플 로그인 반영하여 로그인 및 회원가입 구현

--- src/main/java/app/bottlenote/user/dto/request/OauthRequest.java
+++ src/main/java/app/bottlenote/user/dto/request/OauthRequest.java
@@ -4,20 +4,22 @@ import app.bottlenote.user.constant.GenderType;
 import app.bottlenote.user.constant.SocialType;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
-public record OauthRequest
-	(@NotBlank(message = "EMAIL_NOT_BLANK")
-	 @Email(message = "EMAIL_PATTERN_NOT_VALID")
-	 String email,
+public record OauthRequest(
 
-	 @NotNull(message = "SOCIAL_TYPE_REQUIRED")
-	 SocialType socialType,
+		@Email(message = "EMAIL_PATTERN_NOT_VALID")
+		String email,
 
-	 GenderType gender,
-	 @Min(value = 0, message = "AGE_MINIMUM")
-	 Integer age
-	) {
+		String socialUniqueId,
+
+		@NotNull(message = "SOCIAL_TYPE_REQUIRED")
+		SocialType socialType,
+
+		GenderType gender,
+
+		@Min(value = 0, message = "AGE_MINIMUM")
+		Integer age
+) {
 
 }
