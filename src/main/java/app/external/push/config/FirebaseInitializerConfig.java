1a3612fc83cd54b37bcd6522930585f95c0d056d
hgkim <hgkim@openerd.com>
2025-04-19 00:32:48 +0900

    fix: update Firebase initialization to use ClassPathResource for configuration
    Signed-off-by: hgkim <hgkim@openerd.com>

--- src/main/java/app/external/push/config/FirebaseInitializerConfig.java
+++ src/main/java/app/external/push/config/FirebaseInitializerConfig.java
@@ -7,9 +7,9 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 
-import java.io.ByteArrayInputStream;
-import java.util.Base64;
+import java.io.InputStream;
 
 @Slf4j
 @Configuration
@@ -17,17 +17,16 @@ import java.util.Base64;
 public class FirebaseInitializerConfig {
 	private final FirebaseProperties firebaseProperties;
 
+
 	@Bean
 	public void initialize() {
 		if (FirebaseApp.getApps().isEmpty()) {
 			try {
-				String file = firebaseProperties.getBase64file();
-				byte[] decodeFile = Base64.getDecoder().decode(file);
-				ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(decodeFile);
-
+				ClassPathResource resource = new ClassPathResource(firebaseProperties.getFirebaseConfigurationFile());
+				InputStream serviceAccount = resource.getInputStream();
 				FirebaseOptions options = FirebaseOptions.builder()
-					.setCredentials(GoogleCredentials.fromStream(byteArrayInputStream))
-					.build();
+						.setCredentials(GoogleCredentials.fromStream(serviceAccount))
+						.build();
 				log.info("Firebase 초기화 성공 : {} ", options);
 				FirebaseApp.initializeApp(options);
 			} catch (Exception e) {
