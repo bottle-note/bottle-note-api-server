52be6125ad39cd5a25276646fd73c3bbe9680bc1
hgkim <hgkim@openerd.com>
2025-05-13 21:15:46 +0900

    test: fix
    Signed-off-by: hgkim <hgkim@openerd.com>

--- src/test/java/app/bottlenote/alcohols/service/RegionServiceTest.java
+++ src/test/java/app/bottlenote/alcohols/service/RegionServiceTest.java
@@ -1,7 +1,7 @@
 package app.bottlenote.alcohols.service;
 
 import app.bottlenote.alcohols.dto.response.RegionsItem;
-import app.bottlenote.alcohols.repository.RegionQueryRepository;
+import app.bottlenote.alcohols.repository.JpaRegionQueryRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
@@ -24,18 +24,18 @@ class RegionServiceTest {
 	private AlcoholReferenceService regionService;
 
 	@Mock
-	private RegionQueryRepository regionQueryRepository;
+	private JpaRegionQueryRepository regionQueryRepository;
 
 	@Test
 	@DisplayName("지역목록을 조회 할 수 있다.")
 	void testFindAll() {
 		// given
 		List<RegionsItem> response = List.of(
-			RegionsItem.of(1L, "스코틀랜드/로우랜드", "Scotland/Lowlands", "가벼운 맛이 특징인 로우랜드 위스키"),
-			RegionsItem.of(2L, "스코틀랜드/하이랜드", "Scotland/Highlands", "맛의 다양성이 특징인 하이랜드 위스키, 해안의 짠맛부터 달콤하고 과일 맛까지"),
-			RegionsItem.of(3L, "스코틀랜드/아일랜드", "Scotland/Ireland", "부드러운 맛이 특징인 아일랜드 위스키"),
-			RegionsItem.of(11L, "프랑스", "France", "주로 브랜디와 와인 생산지로 유명하지만 위스키도 생산"),
-			RegionsItem.of(12L, "스웨덴", "Sweden", "실험적인 방법으로 만드는 스웨덴 위스키")
+				RegionsItem.of(1L, "스코틀랜드/로우랜드", "Scotland/Lowlands", "가벼운 맛이 특징인 로우랜드 위스키"),
+				RegionsItem.of(2L, "스코틀랜드/하이랜드", "Scotland/Highlands", "맛의 다양성이 특징인 하이랜드 위스키, 해안의 짠맛부터 달콤하고 과일 맛까지"),
+				RegionsItem.of(3L, "스코틀랜드/아일랜드", "Scotland/Ireland", "부드러운 맛이 특징인 아일랜드 위스키"),
+				RegionsItem.of(11L, "프랑스", "France", "주로 브랜디와 와인 생산지로 유명하지만 위스키도 생산"),
+				RegionsItem.of(12L, "스웨덴", "Sweden", "실험적인 방법으로 만드는 스웨덴 위스키")
 		);
 
 		// When
