338d51827664f5230ed6ec2506f0c5acc0b6f099
KIM HYUN GI <rlagusrl928@gmail.com>
2025-05-14 10:36:20 +0900

    feat: update review detail and retrieval endpoints to use alcoholId instead of reviewId

--- src/main/java/app/bottlenote/review/repository/CustomReviewRepositoryImpl.java
+++ src/main/java/app/bottlenote/review/repository/CustomReviewRepositoryImpl.java
@@ -39,87 +39,87 @@ public class CustomReviewRepositoryImpl implements CustomReviewRepository {
 
 	private static ConstructorExpression<ReviewInfo> composeReviewInfoResult(Long userId) {
 		return Projections.constructor(
-			ReviewInfo.class,
-			// 기본 리뷰 정보
-			review.id,
-			review.content,
-			review.imageUrl,
-			review.createAt,
-			reviewImage.id.countDistinct(),
+				ReviewInfo.class,
+				// 기본 리뷰 정보
+				review.id,
+				review.content,
+				review.imageUrl,
+				review.createAt,
+				reviewImage.id.countDistinct(),
 
-			// 사용자 정보
-			getUserInfo(),
-			isMyReview(userId),
+				// 사용자 정보
+				getUserInfo(),
+				isMyReview(userId),
 
-			// 리뷰 상태 및 속성
-			review.status,
-			review.isBest,
-			review.reviewLocation,
-			review.sizeType,
+				// 리뷰 상태 및 속성
+				review.status,
+				review.isBest,
+				review.reviewLocation,
+				review.sizeType,
 
-			// 가격 및 평점 정보
-			review.price,
-			rating.ratingPoint.rating,
+				// 가격 및 평점 정보
+				review.price,
+				rating.ratingPoint.rating,
 
-			// 좋아요 및 댓글 정보
-			likes.count(),
-			reviewReply.count(),
-			isLikeByMeSubquery(userId),
-			hasReplyByMeSubquery(userId),
+				// 좋아요 및 댓글 정보
+				likes.countDistinct(),
+				reviewReply.countDistinct(),
+				isLikeByMeSubquery(userId),
+				hasReplyByMeSubquery(userId),
 
-			// 기타 정보
-			review.viewCount,
-			getTastingTag()
+				// 기타 정보
+				review.viewCount,
+				getTastingTag()
 		);
 	}
 
 	@Override
 	public ReviewInfo getReview(Long reviewId, Long userId) {
 		return queryFactory.select(composeReviewInfoResult(userId))
-			.from(review)
-			.join(user).on(review.userId.eq(user.id))
-			.leftJoin(likes).on(review.id.eq(likes.reviewId))
-			.leftJoin(alcohol).on(alcohol.id.eq(review.alcoholId))
-			.leftJoin(rating).on(rating.id.alcoholId.eq(review.alcoholId).and(rating.id.userId.eq(review.userId)))
-			.leftJoin(reviewImage).on(review.id.eq(reviewImage.review.id))
-			.leftJoin(reviewReply).on(review.id.eq(reviewReply.reviewId))
-			.where(review.id.eq(reviewId)
-				.and(review.userId.eq(userId).or(review.status.eq(PUBLIC)))
-				.and(review.activeStatus.eq(ACTIVE)))
-			.groupBy(review.id, review.isBest, review.sizeType, review.userId)
-			.fetchOne();
+				.from(review)
+				.join(user).on(review.userId.eq(user.id))
+				.leftJoin(likes).on(review.id.eq(likes.reviewId))
+				.leftJoin(alcohol).on(alcohol.id.eq(review.alcoholId))
+				.leftJoin(rating).on(rating.id.alcoholId.eq(review.alcoholId).and(rating.id.userId.eq(review.userId)))
+				.leftJoin(reviewImage).on(review.id.eq(reviewImage.review.id))
+				.leftJoin(reviewReply).on(review.id.eq(reviewReply.reviewId))
+				.where(review.id.eq(reviewId)
+						.and(review.userId.eq(userId).or(review.status.eq(PUBLIC)))
+						.and(review.activeStatus.eq(ACTIVE)))
+				.groupBy(review.id, review.isBest, review.sizeType, review.userId)
+				.fetchOne();
 	}
 
 	@Override
 	public PageResponse<ReviewListResponse> getReviews(
-		Long alcoholId,
-		ReviewPageableRequest reviewPageableRequest,
-		Long userId
+			Long alcoholId,
+			ReviewPageableRequest reviewPageableRequest,
+			Long userId
 	) {
 		List<ReviewInfo> fetch = queryFactory.select(composeReviewInfoResult(userId))
-			.from(review)
-			.join(user).on(review.userId.eq(user.id))
-			.leftJoin(likes).on(review.id.eq(likes.reviewId))
-			.leftJoin(alcohol).on(alcohol.id.eq(review.alcoholId))
-			.leftJoin(rating).on(rating.id.alcoholId.eq(review.alcoholId).and(rating.id.userId.eq(review.userId)))
-			.leftJoin(reviewReply).on(review.id.eq(reviewReply.reviewId))
-			.leftJoin(reviewImage).on(review.id.eq(reviewImage.review.id))
-			.where(review.alcoholId.eq(alcoholId)
-				.and(review.userId.eq(userId).or(review.status.eq(PUBLIC))) // 내 리뷰는 모두 조회 아니면 공개된 리뷰만 조회
-				.and(review.activeStatus.eq(ACTIVE)))
-			.groupBy(review.id, review.isBest, review.sizeType, review.userId)
-			.orderBy(sortBy(reviewPageableRequest.sortType(), reviewPageableRequest.sortOrder()).toArray(new OrderSpecifier[0]))
-			.offset(reviewPageableRequest.cursor())
-			.limit(reviewPageableRequest.pageSize() + 1)
-			.fetch();
+				.from(review)
+				.join(user).on(review.userId.eq(user.id))
+				.leftJoin(likes).on(review.id.eq(likes.reviewId))
+				.leftJoin(alcohol).on(alcohol.id.eq(review.alcoholId))
+				.leftJoin(rating).on(rating.id.alcoholId.eq(review.alcoholId).and(rating.id.userId.eq(review.userId)))
+				.leftJoin(reviewReply).on(review.id.eq(reviewReply.reviewId))
+				.leftJoin(reviewImage).on(review.id.eq(reviewImage.review.id))
+				.where(review.alcoholId.eq(alcoholId)
+						.and(review.userId.eq(userId).or(review.status.eq(PUBLIC))) // 내 리뷰는 모두 조회 아니면 공개된 리뷰만 조회
+						.and(review.activeStatus.eq(ACTIVE)))
+				.groupBy(review.id, review.isBest, review.sizeType, review.userId)
+				.orderBy(sortBy(reviewPageableRequest.sortType(), reviewPageableRequest.sortOrder()).toArray(new OrderSpecifier[0]))
+				.offset(reviewPageableRequest.cursor())
+				.limit(reviewPageableRequest.pageSize() + 1)
+				.fetch();
 
 		Long totalCount = queryFactory
-			.select(review.id.count())
-			.from(review)
-			.where(review.alcoholId.eq(alcoholId)
-				.and(review.userId.eq(userId).or(review.status.eq(PUBLIC))) // 내 리뷰는 모두 조회 아니면 공개된 리뷰만 조회
-				.and(review.activeStatus.eq(ACTIVE)))
-			.fetchOne();
+				.select(review.id.count())
+				.from(review)
+				.where(review.alcoholId.eq(alcoholId)
+						.and(review.userId.eq(userId).or(review.status.eq(PUBLIC))) // 내 리뷰는 모두 조회 아니면 공개된 리뷰만 조회
+						.and(review.activeStatus.eq(ACTIVE)))
+				.fetchOne();
 
 		CursorPageable cursorPageable = getCursorPageable(reviewPageableRequest, fetch);
 		return PageResponse.of(ReviewListResponse.of(totalCount, fetch), cursorPageable);
@@ -127,36 +127,36 @@ public class CustomReviewRepositoryImpl implements CustomReviewRepository {
 
 	@Override
 	public PageResponse<ReviewListResponse> getReviewsByMe(
-		Long alcoholId,
-		ReviewPageableRequest reviewPageableRequest,
-		Long userId
+			Long alcoholId,
+			ReviewPageableRequest reviewPageableRequest,
+			Long userId
 	) {
 		//특정한 위스키의 내 리뷰만 조회
 		List<ReviewInfo> fetch = queryFactory.select(composeReviewInfoResult(userId))
-			.from(review)
-			.join(alcohol).on(alcohol.id.eq(review.alcoholId).and(alcohol.id.eq(alcoholId)))
-			.join(user).on(review.userId.eq(user.id))
-			.leftJoin(likes).on(review.id.eq(likes.reviewId))
-			.leftJoin(rating).on(rating.id.alcoholId.eq(review.alcoholId).and(rating.id.userId.eq(review.userId)))
-			.leftJoin(reviewReply).on(review.id.eq(reviewReply.reviewId))
-			.leftJoin(reviewImage).on(review.id.eq(reviewImage.review.id))
-			.where(review.userId.eq(userId)
-				.and(review.activeStatus.eq(ACTIVE)))
-			//.and(review.status.eq(PUBLIC)))// 공개 여부와 상관 없이 모두 조회
-			.groupBy(review.id, review.isBest, review.sizeType, review.userId)
-			.orderBy(sortBy(reviewPageableRequest.sortType(), reviewPageableRequest.sortOrder()).toArray(new OrderSpecifier[0]))
-			.offset(reviewPageableRequest.cursor())
-			.limit(reviewPageableRequest.pageSize() + 1)
-			.fetch();
+				.from(review)
+				.join(alcohol).on(alcohol.id.eq(review.alcoholId).and(alcohol.id.eq(alcoholId)))
+				.join(user).on(review.userId.eq(user.id))
+				.leftJoin(likes).on(review.id.eq(likes.reviewId))
+				.leftJoin(rating).on(rating.id.alcoholId.eq(review.alcoholId).and(rating.id.userId.eq(review.userId)))
+				.leftJoin(reviewReply).on(review.id.eq(reviewReply.reviewId))
+				.leftJoin(reviewImage).on(review.id.eq(reviewImage.review.id))
+				.where(review.userId.eq(userId)
+						.and(review.activeStatus.eq(ACTIVE)))
+				//.and(review.status.eq(PUBLIC)))// 공개 여부와 상관 없이 모두 조회
+				.groupBy(review.id, review.isBest, review.sizeType, review.userId)
+				.orderBy(sortBy(reviewPageableRequest.sortType(), reviewPageableRequest.sortOrder()).toArray(new OrderSpecifier[0]))
+				.offset(reviewPageableRequest.cursor())
+				.limit(reviewPageableRequest.pageSize() + 1)
+				.fetch();
 
 		Long totalCount = queryFactory
-			.select(review.id.count())
-			.from(review)
-			.where(review.userId.eq(userId)
-				.and(review.alcoholId.eq(alcoholId))
-				.and(review.activeStatus.eq(ACTIVE))) //
-			//.and(review.status.eq(PUBLIC))) // 공개 여부와 상관 없이 모두 조회
-			.fetchOne();
+				.select(review.id.count())
+				.from(review)
+				.where(review.userId.eq(userId)
+						.and(review.alcoholId.eq(alcoholId))
+						.and(review.activeStatus.eq(ACTIVE))) //
+				//.and(review.status.eq(PUBLIC))) // 공개 여부와 상관 없이 모두 조회
+				.fetchOne();
 
 		CursorPageable cursorPageable = getCursorPageable(reviewPageableRequest, fetch);
 		return PageResponse.of(ReviewListResponse.of(totalCount, fetch), cursorPageable);
