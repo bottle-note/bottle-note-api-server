1a17092219bf3642ddbc65e35389dc7199f011b4
hgkim <hgkim@openerd.com>
2025-06-19 21:07:42 +0900

    feat: add BusinessSupportController with initial setup
    Signed-off-by: hgkim <hgkim@openerd.com>

---
+++ src/main/java/app/bottlenote/support/business/controller/BusinessSupportController.java
@@ -0,0 +1,12 @@
+package app.bottlenote.support.business.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/business-support")
+@RequiredArgsConstructor
+public class BusinessSupportController {
+//todo : need to implement the business support controller methods
+}
