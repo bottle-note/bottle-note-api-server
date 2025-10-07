929dc08cb235e58756298ec114980a119654ea77
hgkim <rlagusrl928@gmail.com>
2025-10-08 00:45:46 +0900

    feat: update GitHub Actions workflow for improved documentation generation and add necessary dependencies

--- bottlenote-admin-api/build.gradle.kts
+++ bottlenote-admin-api/build.gradle.kts
@@ -1,21 +1,24 @@
+// bottlenote-admin-api
 plugins {
-    alias(libs.plugins.kotlin.jvm)
-    alias(libs.plugins.kotlin.spring)
+	alias(libs.plugins.kotlin.jvm)
+	alias(libs.plugins.kotlin.spring)
+	alias(libs.plugins.asciidoctor)
+	alias(libs.plugins.restdocs.api.spec)
 }
 
 dependencies {
-    implementation(project(":bottlenote-mono"))
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.springframework.boot:spring-boot-starter-validation")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation(project(":bottlenote-mono"))
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
 }
 
 tasks.bootJar {
-    enabled = true
-    archiveFileName.set("bottlenote-admin-api.jar")
+	enabled = true
+	archiveFileName.set("bottlenote-admin-api.jar")
 }
 
 tasks.jar {
-    enabled = true
+	enabled = true
 }
