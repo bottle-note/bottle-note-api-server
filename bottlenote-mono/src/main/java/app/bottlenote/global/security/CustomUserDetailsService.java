abe7e5bfc7accc905c7d3aaff41b872986a93bab
hgkim <rlagusrl928@gmail.com>
2025-10-08 13:17:36 +0900

    feat: change CustomUserDetailsService annotation from @Service to @Component

--- bottlenote-mono/src/main/java/app/bottlenote/global/security/CustomUserDetailsService.java
+++ bottlenote-mono/src/main/java/app/bottlenote/global/security/CustomUserDetailsService.java
@@ -11,10 +11,10 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
-@Service
+@Component
 @RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
 
