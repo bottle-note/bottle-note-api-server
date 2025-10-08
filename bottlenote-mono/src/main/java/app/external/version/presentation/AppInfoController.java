8896734894683bcaad11176b08d96dba6b8260b3
hgkim <rlagusrl928@gmail.com>
2025-10-09 00:00:30 +0900

    feat: enhance AppInfoController to include shortened and full git commit hashes, add logging for build time parsing errors

--- bottlenote-mono/src/main/java/app/external/version/presentation/AppInfoController.java
+++ bottlenote-mono/src/main/java/app/external/version/presentation/AppInfoController.java
@@ -8,11 +8,13 @@ import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/v1/app-info")
 @RequiredArgsConstructor
@@ -25,7 +27,8 @@ public class AppInfoController {
     infoMap.put("serverName", info.getServerName());
     infoMap.put("environment", info.getEnvironment());
     infoMap.put("gitBranch", info.getGitBranch());
-    infoMap.put("gitCommit", info.getGitCommit());
+    infoMap.put("gitCommitHash", info.getGitCommit().substring(0, 7));
+    infoMap.put("gitCommitFullHash", info.getGitCommit());
 
     String buildTime = info.getGitBuildTime();
     try {
@@ -34,6 +37,7 @@ public class AppInfoController {
               .withZoneSameInstant(ZoneId.of("Asia/Seoul"))
               .format(DateTimeFormatter.ISO_OFFSET_DATE_TIME);
     } catch (Exception ignored) {
+      log.warn("deploy time parse error but not critical");
     }
     infoMap.put("gitBuildTime", buildTime);
 
