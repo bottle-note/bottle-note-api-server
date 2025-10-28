cd364ef8e0147d1722613066af9dc50f0282fecb
rlagu <rlagusrl928@gmail.com>
2025-10-29 00:31:06 +0900

    fix: replace @JoinColumn with @Column annotations for Hibernate compatibility

--- bottlenote-mono/src/main/java/app/bottlenote/picks/domain/Picks.java
+++ bottlenote-mono/src/main/java/app/bottlenote/picks/domain/Picks.java
@@ -4,13 +4,13 @@ import static lombok.AccessLevel.PROTECTED;
 
 import app.bottlenote.common.domain.BaseTimeEntity;
 import app.bottlenote.picks.constant.PicksStatus;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
