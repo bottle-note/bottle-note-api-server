a499c075dab1c6c54c5a911cd26dc1863a3d8746
hgkim <rlagusrl928@gmail.com>
2025-12-20 15:32:05 +0900

    chore: Product API 및 Admin API 설정 추가 및 버전 업데이트

--- bottlenote-mono/src/main/java/app/bottlenote/history/scheduled/ViewHistoryJobConfig.java
+++ bottlenote-mono/src/main/java/app/bottlenote/history/scheduled/ViewHistoryJobConfig.java
@@ -6,10 +6,15 @@ import org.quartz.JobBuilder;
 import org.quartz.JobDetail;
 import org.quartz.Trigger;
 import org.quartz.TriggerBuilder;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ConditionalOnProperty(
+    name = "schedules.history.view.sync.enable",
+    havingValue = "true",
+    matchIfMissing = true)
 @RequiredArgsConstructor
 public class ViewHistoryJobConfig {
 
